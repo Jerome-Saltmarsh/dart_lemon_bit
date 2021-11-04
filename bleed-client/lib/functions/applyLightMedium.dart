5c0e494acc9c3bb8e999ecf751a86c657a471a44
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-11-04 12:11:46 +0100

    saving

--- bleed-client/lib/functions/applyLightMedium.dart
+++ bleed-client/lib/functions/applyLightMedium.dart
@@ -1,90 +1,33 @@
 import 'package:bleed_client/enums/Shading.dart';
 import 'package:bleed_client/functions/applyShade.dart';
-import 'package:bleed_client/state/game.dart';
 import 'package:bleed_client/getters/getTileAt.dart';
 
 void applyLightMedium(List<List<Shading>> shader, double x, double y) {
   int column = getColumn(x, y);
   int row = getRow(x, y);
-
   applyShadeMedium(shader, row, column);
-
-  if (row > 1) {
-    applyShadeDark(shader, row - 2, column);
-    if (column > 0) {
-      applyShadeDark(shader, row - 2, column - 1);
-    }
-    if (column > 1) {
-      applyShadeDark(shader, row - 2, column - 2);
-    }
-    if (column < game.totalColumns - 1) {
-      applyShadeDark(shader, row - 2, column + 1);
-    }
-    if (column < game.totalColumns - 2) {
-      applyShadeDark(shader, row - 2, column + 2);
-    }
-  }
-  if (row < game.totalRows - 2) {
-    applyShadeDark(shader, row + 2, column);
-
-    if (column > 0) {
-      applyShadeDark(shader, row + 2, column - 1);
-    }
-    if (column > 1) {
-      applyShadeDark(shader, row + 2, column - 2);
-    }
-    if (column < game.totalColumns - 1) {
-      applyShadeDark(shader, row + 2, column + 1);
-    }
-    if (column < game.totalColumns - 2) {
-      applyShadeDark(shader, row + 2, column + 2);
-    }
-  }
-
-  if (column > 0) {
-    applyShadeDark(shader, row, column - 2);
-
-    if (row > 0) {
-      applyShadeDark(shader, row - 1, column - 2);
-    }
-    if (row < game.totalRows - 1) {
-      applyShadeDark(shader, row + 1, column - 2);
-    }
-  }
-  if (column < game.totalColumns - 1) {
-    applyShadeDark(shader, row, column + 2);
-
-    if (row > 0) {
-      applyShadeDark(shader, row - 1, column + 2);
-    }
-    if (row < game.totalRows - 1) {
-      applyShadeDark(shader, row + 1, column + 2);
-    }
-  }
-
-  if (row > 0) {
-    applyShadeMedium(shader, row - 1, column);
-    if (column > 0) {
-      applyShadeMedium(shader, row - 1, column - 1);
-    }
-    if (column + 1 < game.totalColumns) {
-      applyShadeMedium(shader, row - 1, column + 1);
-    }
-  }
-  if (column > 0) {
-    applyShadeMedium(shader, row, column - 1);
-  }
-  if (column + 1 < game.totalColumns) {
-    applyShadeMedium(shader, row, column + 1);
-    if (row + 1 < game.totalRows) {
-      applyShadeMedium(shader, row + 1, column + 1);
-    }
-  }
-  if (row + 1 < game.totalRows) {
-    applyShadeMedium(shader, row + 1, column);
-
-    if (column > 0) {
-      applyShadeMedium(shader, row + 1, column - 1);
-    }
-  }
+  applyShadeDark(shader, row - 2, column);
+  applyShadeDark(shader, row - 2, column - 1);
+  applyShadeDark(shader, row - 2, column - 2);
+  applyShadeDark(shader, row - 2, column + 1);
+  applyShadeDark(shader, row - 2, column + 2);
+  applyShadeDark(shader, row + 2, column);
+  applyShadeDark(shader, row + 2, column - 1);
+  applyShadeDark(shader, row + 2, column - 2);
+  applyShadeDark(shader, row + 2, column + 1);
+  applyShadeDark(shader, row + 2, column + 2);
+  applyShadeDark(shader, row, column - 2);
+  applyShadeDark(shader, row - 1, column - 2);
+  applyShadeDark(shader, row + 1, column - 2);
+  applyShadeDark(shader, row, column + 2);
+  applyShadeDark(shader, row - 1, column + 2);
+  applyShadeDark(shader, row + 1, column + 2);
+  applyShadeMedium(shader, row - 1, column);
+  applyShadeMedium(shader, row - 1, column - 1);
+  applyShadeMedium(shader, row - 1, column + 1);
+  applyShadeMedium(shader, row, column - 1);
+  applyShadeMedium(shader, row, column + 1);
+  applyShadeMedium(shader, row + 1, column + 1);
+  applyShadeMedium(shader, row + 1, column);
+  applyShadeMedium(shader, row + 1, column - 1);
 }
