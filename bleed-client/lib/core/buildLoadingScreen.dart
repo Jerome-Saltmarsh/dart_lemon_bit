307be5bc69321c6d9ba3b4e3c9ddacf57897da51
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-11-18 09:04:05 +0100

    saving

--- bleed-client/lib/core/buildLoadingScreen.dart
+++ bleed-client/lib/core/buildLoadingScreen.dart
@@ -1,14 +1,40 @@
 import 'package:bleed_client/ui/compose/widgets.dart';
+import 'package:bleed_client/ui/state/flutter_constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lemon_watch/watch.dart';
 import 'package:lemon_watch/watch_builder.dart';
 
 Watch<double> download = Watch(0);
+const double _width = 300;
+const double _height = 50;
 
 Widget buildLoadingScreen(BuildContext context) {
-  return WatchBuilder(download, (double value){
+  return WatchBuilder(download, (double value) {
     print("download $value");
-    return text("DOWNLOADING ${(value * 100).toInt()}%", color: Colors.black);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          mainAxisAlignment: main.center,
+          children: [
+            text("DOWNLOADING ${(value * 100).toInt()}%", color: Colors.black),
+            Container(
+              width: _width,
+              height: _height,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+              alignment: Alignment.centerLeft,
+              child: Container(
+                width: _width * value,
+                height: _height,
+              ),
+            )
+          ],
+        ),
+      ],
+    );
   });
 }
