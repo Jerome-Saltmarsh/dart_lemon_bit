726e4294677883434317bbc5912e392cbc355a83
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-11-22 14:38:47 +0100

    saving

--- bleed-client/lib/mappers/mapParticleToRSTransform.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:bleed_client/classes/Particle.dart';
-import 'package:flutter/cupertino.dart';
-
-import '../draw.dart';
-
-RSTransform mapParticleToRSTransform(Particle particle){
-  return rsTransform(x: particle.x, y: particle.y + particle.z, anchorX: 32, anchorY: 32, scale: particle.scale);
-}
