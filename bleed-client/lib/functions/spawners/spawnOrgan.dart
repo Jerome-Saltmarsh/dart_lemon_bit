aa8fe8e5bc6906019a5111e4ff08fb175a6d0dc6
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-11-24 09:18:15 +0100

    saving

--- bleed-client/lib/functions/spawners/spawnOrgan.dart
+++ bleed-client/lib/functions/spawners/spawnOrgan.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:bleed_client/enums/ParticleType.dart';
+import 'package:lemon_math/give_or_take.dart';
 import 'package:lemon_math/randomInt.dart';
 import 'package:lemon_math/random_between.dart';
 
@@ -15,8 +18,8 @@ void spawnOrgan(double x, double y, double z, {double xv = 0, double yv = 0}) {
       zv: randomBetween(0, 0.03),
       weight: 0.25,
       duration: randomInt(90, 150),
-      rotation: 0,
-      rotationV: 0,
+      rotation: giveOrTake(pi),
+      rotationV: 100,
       scale: 1,
       scaleV: 0);
 }
