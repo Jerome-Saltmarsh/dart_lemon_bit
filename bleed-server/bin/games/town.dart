ad3ca07bde4221dbd985807603ab411d00ec4ef1
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-11-26 16:17:37 +0100

    saving

--- bleed-server/bin/games/town.dart
+++ bleed-server/bin/games/town.dart
@@ -17,7 +17,7 @@ class Town extends Game {
   late InteractableNpc guard1;
   late InteractableNpc guard2;
 
-  final int _maxZombies = 0;
+  final int _maxZombies = 5;
   final int _framesPerZombieSpawn = 5;
 
   Town() : super(scenes.town) {
