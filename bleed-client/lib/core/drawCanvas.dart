ae865176f5793126cc3efb5ddc96c84475c71b67
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2021-12-28 12:51:16 +1100

    saving

--- bleed-client/lib/core/drawCanvas.dart
+++ bleed-client/lib/core/drawCanvas.dart
@@ -7,18 +7,18 @@ import 'package:bleed_client/common/GameStatus.dart';
 import 'package:bleed_client/common/GameType.dart';
 import 'package:bleed_client/common/enums/Direction.dart';
 import 'package:bleed_client/editor/render/drawEditor.dart';
+import 'package:bleed_client/enums/Region.dart';
 import 'package:bleed_client/images.dart';
 import 'package:bleed_client/render/draw/drawAtlas.dart';
 import 'package:bleed_client/render/mappers/mapArcherToSrc.dart';
 import 'package:bleed_client/render/mappers/mapDst.dart';
+import 'package:bleed_client/render/mappers/mapSrcWitch.dart';
 import 'package:bleed_client/webSocket.dart';
 import 'package:bleed_client/render/draw/drawCanvas.dart';
 import 'package:bleed_client/state/game.dart';
 import 'package:bleed_client/watches/mode.dart';
 import 'package:flutter/material.dart';
 import 'package:lemon_engine/game.dart';
-import 'package:lemon_engine/render/draw_circle.dart';
-import 'package:lemon_engine/state/paint.dart';
 
 void drawCanvas(Canvas canvas, Size size) {
 
@@ -28,8 +28,12 @@ void drawCanvas(Canvas canvas, Size size) {
     return;
   }
 
+  if (game.region.value == Region.None){
+    renderCanvasSelectRegion();
+    return;
+  }
   if (game.type.value == GameType.None) {
-    renderSelectGameCanvas(canvas, size);
+    renderCanvasSelectGame();;
     return;
   }
   if (!webSocket.connected) return;
@@ -39,13 +43,24 @@ void drawCanvas(Canvas canvas, Size size) {
 }
 
 int direction = 0;
-final Duration frameRate = Duration(milliseconds: 60);
+final Duration _frameRate = Duration(milliseconds: 100);
 
-void renderSelectGameCanvas(Canvas canvas, Size size){
+void renderCanvasSelectGame(){
   direction = (direction + 1) % directions.length;
   drawArcher(x: 50, y: 100, state: CharacterState.Idle, direction: directions[direction], frame: 1, scale: 1);
-  Future.delayed(frameRate, redrawCanvas);
+  _redraw();
 }
+
+void renderCanvasSelectRegion(){
+  direction = (direction + 1) % directions.length;
+  drawWitch(x: 50, y: 100, state: CharacterState.Idle, direction: directions[direction], frame: 1, scale: 1);
+  _redraw();
+}
+
+void _redraw(){
+  Future.delayed(_frameRate, redrawCanvas);
+}
+
 
 void drawArcher({
   required double x,
@@ -57,3 +72,15 @@ void drawArcher({
 }){
   drawAtlas(mapDst(x: x, y: y, scale: scale), mapSrcArcher(state: state, direction: direction, frame: frame));
 }
+
+void drawWitch({
+  required double x,
+  required double y,
+  required CharacterState state,
+  required Direction direction,
+  required int frame,
+  double scale = 1,
+}){
+  drawAtlas(mapDst(x: x, y: y, scale: scale), mapSrcWitch(state: state, direction: direction, frame: frame));
+}
+
