68d40b8d65dd0c71098ba7dea1b254407b2e4cea
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-01-05 10:54:12 +1000

    saving

--- gamestream-users/bin/server.dart
+++ gamestream-users/bin/server.dart
@@ -2,13 +2,26 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
+import 'package:googleapis/firestore/v1.dart';
+import 'package:googleapis_auth/auth_io.dart';
+
+import 'helpers.dart';
+
+// https://github.com/dart-lang/samples/tree/master/server/google_apis
 void main() async {
   var handler =
   const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
-  var server = await shelf_io.serve(handler, 'localhost', 8082);
+  var server = await shelf_io.serve(handler, '0.0.0.0', 8080);
   // Enable content compression
   server.autoCompress = true;
   print('Serving at http://${server.address.host}:${server.port}');
+
+  // final projectId = await currentProjectId();
+  // print('Current GCP project id: $projectId');
+
+  // final authClient = await clientViaApplicationDefaultCredentials(
+  //   scopes: [FirestoreApi.datastoreScope],
+  // );
 }
 
 Response _echoRequest(Request request){
@@ -17,9 +30,10 @@ Response _echoRequest(Request request){
     case "webhook":
       print("handling webhook");
       return Response.ok('Request for "${request.url}"');
-    default:
-      print("Cannot handle request $path");
+    case "project":
       return Response.ok('Request for "${request.url}"');
+    default:
+      return Response.ok('Cannot handle request "${request.url}"');
   }
 }
 
