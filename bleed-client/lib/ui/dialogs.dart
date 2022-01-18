315a8eef75314ee413c2ce9768b6841250a257e5
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-01-19 08:19:42 +1000

    saving

--- bleed-client/lib/ui/dialogs.dart
+++ bleed-client/lib/ui/dialogs.dart
@@ -343,8 +343,8 @@ Widget buildDialogConfirmCancelSubscription(){
     width: style.dialogWidthMedium,
     height: style.dialogHeightSmall,
     child: Center(child: text("Cancel premium subscription?", color: colours.white90)),
-    bottomLeft: button(text("YES", color: colours.red, bold: true), actions.cancelSubscription, fillColor: none, borderColor: colours.red, borderWidth: 2, width: 100),
-    bottomRight: buildButton("NO", actions.showDialogAccount),
+    bottomLeft: button(text("YES", color: colours.red, bold: false), actions.cancelSubscription, fillColor: none, borderColor: colours.none, width: 100),
+    bottomRight: button(text("NO", color: colours.green, bold: true), actions.showDialogAccount, fillColor: none, borderColor: colours.green, width: 100),
   );
 }
 
