6958065628c5d42cc146a54ad0dc80f1ea34fd55
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-01-25 08:57:26 +1000

    saving

--- bleed-client/lib/modules/core/buildLoadingScreen.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bleed_client/constants/colours.dart';
-import 'package:bleed_client/flutterkit.dart';
-import 'package:bleed_client/styles.dart';
-import 'package:flutter/material.dart';
-import 'package:lemon_watch/watch.dart';
-import 'package:lemon_watch/watch_builder.dart';
-
-
