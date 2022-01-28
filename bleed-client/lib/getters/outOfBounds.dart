58f080b7adeaac2e63a1fba90536880c3833fc50
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-01-28 12:44:15 +1000

    saving

--- bleed-client/lib/getters/outOfBounds.dart
+++ bleed-client/lib/getters/outOfBounds.dart
@@ -1,10 +1,12 @@
 
 import 'package:bleed_client/modules/modules.dart';
 
+final _state = modules.isometric.state;
+
 bool outOfBounds(int row, int column){
   if (row < 0) return true;
   if (column < 0) return true;
-  if (row >= modules.isometric.state.totalRows.value) return true;
-  if (column >= modules.isometric.state.totalColumns.value) return true;
+  if (row >= _state.totalRowsInt) return true;
+  if (column >= _state.totalColumnsInt) return true;
   return false;
 }
