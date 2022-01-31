062499a8ae38d54e28ef99fac97374eecbb6a6c6
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-01-31 15:21:29 +1000

    saving

--- bleed-client/lib/modules/game/build.dart
+++ bleed-client/lib/modules/game/build.dart
@@ -1,14 +1,28 @@
 
+import 'dart:math';
+
+import 'package:bleed_client/common/CharacterType.dart';
 import 'package:bleed_client/common/GameStatus.dart';
 import 'package:bleed_client/common/GameType.dart';
+import 'package:bleed_client/common/WeaponType.dart';
+import 'package:bleed_client/constants/colours.dart';
 import 'package:bleed_client/flutterkit.dart';
+import 'package:bleed_client/modules/modules.dart';
+import 'package:bleed_client/send.dart';
 import 'package:bleed_client/state/game.dart';
+import 'package:bleed_client/styles.dart';
+import 'package:bleed_client/ui/compose/buildTextBox.dart';
 import 'package:bleed_client/ui/compose/hudUI.dart';
 import 'package:bleed_client/ui/dialogs.dart';
 import 'package:bleed_client/ui/state/hud.dart';
 import 'package:bleed_client/ui/style.dart';
 import 'package:bleed_client/ui/views.dart';
+import 'package:bleed_client/ui/widgets.dart';
+import 'package:bleed_client/utils/widget_utils.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:golden_ratio/constants.dart';
+import 'package:lemon_engine/engine.dart';
 import 'package:lemon_watch/watch_builder.dart';
 
 import '../../toString.dart';
@@ -39,13 +53,13 @@ class GameBuild {
           case GameStatus.In_Progress:
             switch (game.type.value) {
               case GameType.MMO:
-                return buildHud.playerCharacterType();
+                return playerCharacterType();
               case GameType.Custom:
-                return buildHud.playerCharacterType();
+                return playerCharacterType();
               case GameType.Moba:
-                return buildHud.playerCharacterType();
+                return playerCharacterType();
               case GameType.BATTLE_ROYAL:
-                return buildHud.playerCharacterType();
+                return playerCharacterType();
               case GameType.CUBE3D:
                 return buildUI3DCube();
               default:
@@ -59,4 +73,234 @@ class GameBuild {
       });
     });
   }
+
+  Widget playerCharacterType() {
+    return WatchBuilder(modules.game.state.player.characterType, (CharacterType value) {
+      if (value == CharacterType.Human) {
+        return _buildHudWeapons();
+      }
+      return _buildHudAbilities();
+    });
+  }
+
+  Widget _buildHudWeapons(){
+    return WatchBuilder(modules.game.state.player.weaponType, (WeaponType weaponType){
+      return layout(
+          padding: 16,
+          topLeft: buildTime(),
+          topRight: buttons.exit,
+          bottomLeft: buildWeaponMenu(),
+      );
+    });
+  }
+
+
+  Widget _buildHudAbilities(){
+    return WatchBuilder(modules.game.state.player.alive, (bool alive) {
+      return Stack(
+        children: [
+          buildTextBox(),
+          if (alive) buildBottomRight(),
+          buildTopLeft(),
+          if (alive) buildBottomCenter(),
+          if (!hud.state.observeMode && !alive) _buildViewRespawn(),
+          if (!alive && hud.state.observeMode) _buildRespawnLight(),
+          _buildServerText(),
+          buildTopRight(),
+          buildNumberOfPlayersRequiredDialog(),
+          // bottomLeft(child: fps),
+        ],
+      );
+    });
+  }
+
+  Widget _buildViewRespawn() {
+    print("buildViewRespawn()");
+    return Container(
+      width: engine.state.screen.width,
+      height: engine.state.screen.height,
+      child: Row(
+        mainAxisAlignment: axis.main.center,
+        crossAxisAlignment: axis.cross.center,
+        children: [
+          Container(
+              padding: padding16,
+              width: max(engine.state.screen.width * goldenRatio_0381, 480),
+              decoration: BoxDecoration(
+                  borderRadius: borderRadius4, color: Colors.black38),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: axis.cross.center,
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: borderRadius4,
+                          color: colours.blood,
+                        ),
+                        padding: padding8,
+                        child: text("BLEED beta v1.0.0")),
+                    height16,
+                    text("YOU DIED", size: 30, underline: true),
+                    height16,
+                    Container(
+                      padding: padding16,
+                      decoration: BoxDecoration(
+                        borderRadius: borderRadius4,
+                        color: black26,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: axis.cross.center,
+                        children: [
+                          text("Please Support Me"),
+                          height16,
+                          Row(
+                            mainAxisAlignment: axis.main.even,
+                            children: [
+                              onPressed(
+                                child: border(
+                                    child: Container(
+                                        width: 70,
+                                        alignment: Alignment.center,
+                                        child: text(
+                                          "Paypal",
+                                        )),
+                                    radius: borderRadius4,
+                                    padding: padding8),
+                                callback: () {
+                                  // openLink(links.paypal);
+                                },
+                                // hint: links.paypal
+                              ),
+                              onPressed(
+                                child: border(
+                                    child: Container(
+                                        width: 70,
+                                        alignment: Alignment.center,
+                                        child: text("Patreon")),
+                                    radius: borderRadius4,
+                                    padding: padding8),
+                                callback: () {
+                                  // openLink(links.patreon);
+                                },
+                                // hint: links.patreon
+                              )
+                            ],
+                          ),
+                          height8,
+                        ],
+                      ),
+                    ),
+                    height8,
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: borderRadius4,
+                        color: black26,
+                      ),
+                      padding: padding16,
+                      child: Column(
+                        children: [
+                          text("Hints"),
+                          Row(
+                            mainAxisAlignment: axis.main.center,
+                            crossAxisAlignment: axis.cross.center,
+                            children: [
+                              Container(
+                                  width: 350,
+                                  alignment: Alignment.center,
+                                  child: text(hud.currentTip)),
+                              width16,
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    height32,
+                    Row(
+                      mainAxisAlignment: axis.main.between,
+                      children: [
+                        onPressed(
+                            child: Container(
+                                padding: padding16, child: text("Close")),
+                            callback: () {
+                              hud.state.observeMode = true;
+                            }),
+                        width16,
+                        mouseOver(
+                            builder: (BuildContext context, bool mouseOver) {
+                              return onPressed(
+                                child: border(
+                                    child: text(
+                                      "RESPAWN",
+                                      weight: bold,
+                                    ),
+                                    padding: padding16,
+                                    radius: borderRadius4,
+                                    color: Colors.white,
+                                    borderWidth: 1,
+                                    fillColor: mouseOver ? black54 : black26),
+                                callback: sendRequestRevive,
+                                hint: "Click to respawn",
+                              );
+                            })
+                      ],
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildServerText() {
+    return WatchBuilder(modules.game.state.player.message, (String value) {
+      if (value.isEmpty) return blank;
+
+      return Positioned(
+          child: Container(
+            width: engine.state.screen.width,
+            alignment: Alignment.center,
+            child: Container(
+              width: 300,
+              color: Colors.black45,
+              padding: padding16,
+              child: Column(
+                children: [
+                  text(modules.game.state.player.message.value),
+                  height16,
+                  button("Next", clearPlayerMessage),
+                ],
+              ),
+            ),
+          ),
+          bottom: 100);
+    });
+  }
+
+  Positioned _buildRespawnLight() {
+    return Positioned(
+        top: 30,
+        child: Container(
+            width: engine.state.screen.width,
+            child: Column(
+              crossAxisAlignment: axis.cross.center,
+              children: [
+                Row(mainAxisAlignment: axis.main.center, children: [
+                  onPressed(
+                      callback: () {
+                        sendRequestRevive();
+                        hud.state.observeMode = false;
+                      },
+                      child: border(
+                          child: text("Respawn", size: 30),
+                          padding: padding8,
+                          radius: borderRadius4))
+                ]),
+                height32,
+                text("Hold E to pan camera")
+              ],
+            )));
+  }
+
+
 }
--- bleed-client/lib/ui/state/hud.dart
+++ bleed-client/lib/ui/state/hud.dart
@@ -2,29 +2,16 @@
 
 import 'dart:math';
 
-import 'package:bleed_client/common/CharacterType.dart';
-import 'package:bleed_client/common/WeaponType.dart';
-import 'package:bleed_client/constants/colours.dart';
 import 'package:bleed_client/flutterkit.dart';
 import 'package:bleed_client/modules/modules.dart';
-import 'package:bleed_client/state/game.dart';
-import 'package:bleed_client/ui/compose/buildTextBox.dart';
-import 'package:bleed_client/ui/compose/hudUI.dart';
-import 'package:bleed_client/utils/widget_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:golden_ratio/constants.dart';
 import 'package:lemon_engine/engine.dart';
 import 'package:lemon_math/pi2.dart';
-import 'package:lemon_watch/watch.dart';
 import 'package:lemon_watch/watch_builder.dart';
 
-import '../../send.dart';
-import '../../styles.dart';
-import '../widgets.dart';
 import 'tips.dart';
 
 final _Hud hud = _Hud();
-final _BuildHud buildHud = _BuildHud();
 
 class _Hud {
   final _State state = _State();
@@ -38,50 +25,6 @@ extension HudProperties on _Hud {
   bool get textFieldFocused => modules.game.state.textFieldMessage.hasPrimaryFocus;
 }
 
-class _BuildHud {
-  Widget playerCharacterType() {
-    return WatchBuilder(modules.game.state.player.characterType, (CharacterType value) {
-      if (value == CharacterType.Human) {
-        return _buildHudWeapons();
-      }
-      return _buildHudAbilities();
-    });
-  }
-}
-
-Widget _buildHudWeapons(){
-  return WatchBuilder(modules.game.state.player.weaponType, (WeaponType weaponType){
-    return layout(
-      padding: 16,
-      topLeft: buildTime(),
-      topRight: buttons.exit,
-      bottomLeft: buildWeaponMenu(),
-      bottomRight: Refresh((){
-        return text("zoom ${engine.state.zoom}, target: ${engine.state.targetZoom}");
-      })
-    );
-  });
-}
-
-
-Widget _buildHudAbilities(){
-  return WatchBuilder(modules.game.state.player.alive, (bool alive) {
-    return Stack(
-      children: [
-        buildTextBox(),
-        if (alive) buildBottomRight(),
-        buildTopLeft(),
-        if (alive) buildBottomCenter(),
-        if (!hud.state.observeMode && !alive) _buildViewRespawn(),
-        if (!alive && hud.state.observeMode) _buildRespawnLight(),
-        _buildServerText(),
-        buildTopRight(),
-        buildNumberOfPlayersRequiredDialog(),
-        // bottomLeft(child: fps),
-      ],
-    );
-  });
-}
 
 final Widget fps = WatchBuilder(engine.state.fps, (int fps){
   return text("fps $fps");
@@ -117,190 +60,5 @@ class Ring {
   }
 }
 
-Positioned _buildRespawnLight() {
-  return Positioned(
-      top: 30,
-      child: Container(
-          width: engine.state.screen.width,
-          child: Column(
-            crossAxisAlignment: axis.cross.center,
-            children: [
-              Row(mainAxisAlignment: axis.main.center, children: [
-                onPressed(
-                    callback: () {
-                      sendRequestRevive();
-                      hud.state.observeMode = false;
-                    },
-                    child: border(
-                        child: text("Respawn", size: 30),
-                        padding: padding8,
-                        radius: borderRadius4))
-              ]),
-              height32,
-              text("Hold E to pan camera")
-            ],
-          )));
-}
 
-Widget _buildServerText() {
-  return WatchBuilder(modules.game.state.player.message, (String value) {
-    if (value.isEmpty) return blank;
 
-    return Positioned(
-        child: Container(
-          width: engine.state.screen.width,
-          alignment: Alignment.center,
-          child: Container(
-            width: 300,
-            color: Colors.black45,
-            padding: padding16,
-            child: Column(
-              children: [
-                text(modules.game.state.player.message.value),
-                height16,
-                button("Next", clearPlayerMessage),
-              ],
-            ),
-          ),
-        ),
-        bottom: 100);
-  });
-}
-
-Widget _buildViewRespawn() {
-  print("buildViewRespawn()");
-  return Container(
-    width: engine.state.screen.width,
-    height: engine.state.screen.height,
-    child: Row(
-      mainAxisAlignment: axis.main.center,
-      crossAxisAlignment: axis.cross.center,
-      children: [
-        Container(
-            padding: padding16,
-            width: max(engine.state.screen.width * goldenRatio_0381, 480),
-            decoration: BoxDecoration(
-                borderRadius: borderRadius4, color: Colors.black38),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: axis.cross.center,
-                children: [
-                  Container(
-                      decoration: BoxDecoration(
-                        borderRadius: borderRadius4,
-                        color: colours.blood,
-                      ),
-                      padding: padding8,
-                      child: text("BLEED beta v1.0.0")),
-                  height16,
-                  text("YOU DIED", size: 30, decoration: underline),
-                  height16,
-                  Container(
-                    padding: padding16,
-                    decoration: BoxDecoration(
-                      borderRadius: borderRadius4,
-                      color: black26,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: axis.cross.center,
-                      children: [
-                        text("Please Support Me"),
-                        height16,
-                        Row(
-                          mainAxisAlignment: axis.main.even,
-                          children: [
-                            onPressed(
-                              child: border(
-                                  child: Container(
-                                      width: 70,
-                                      alignment: Alignment.center,
-                                      child: text(
-                                        "Paypal",
-                                      )),
-                                  radius: borderRadius4,
-                                  padding: padding8),
-                              callback: () {
-                                // openLink(links.paypal);
-                              },
-                              // hint: links.paypal
-                            ),
-                            onPressed(
-                              child: border(
-                                  child: Container(
-                                      width: 70,
-                                      alignment: Alignment.center,
-                                      child: text("Patreon")),
-                                  radius: borderRadius4,
-                                  padding: padding8),
-                              callback: () {
-                                // openLink(links.patreon);
-                              },
-                              // hint: links.patreon
-                            )
-                          ],
-                        ),
-                        height8,
-                      ],
-                    ),
-                  ),
-                  height8,
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: borderRadius4,
-                      color: black26,
-                    ),
-                    padding: padding16,
-                    child: Column(
-                      children: [
-                        text("Hints"),
-                        Row(
-                          mainAxisAlignment: axis.main.center,
-                          crossAxisAlignment: axis.cross.center,
-                          children: [
-                            Container(
-                                width: 350,
-                                alignment: Alignment.center,
-                                child: text(hud.currentTip)),
-                            width16,
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  height32,
-                  Row(
-                    mainAxisAlignment: axis.main.between,
-                    children: [
-                      onPressed(
-                          child: Container(
-                              padding: padding16, child: text("Close")),
-                          callback: () {
-                            hud.state.observeMode = true;
-                          }),
-                      width16,
-                      mouseOver(
-                          builder: (BuildContext context, bool mouseOver) {
-                            return onPressed(
-                              child: border(
-                                  child: text(
-                                    "RESPAWN",
-                                    weight: bold,
-                                  ),
-                                  padding: padding16,
-                                  radius: borderRadius4,
-                                  color: Colors.white,
-                                  borderWidth: 1,
-                                  fillColor: mouseOver ? black54 : black26),
-                              callback: sendRequestRevive,
-                              hint: "Click to respawn",
-                            );
-                          })
-                    ],
-                  ),
-                ],
-              ),
-            )),
-      ],
-    ),
-  );
-}
