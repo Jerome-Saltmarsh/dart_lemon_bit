278c54583d952fcdf631ad2e0d681357cc9c8f72
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-02-04 08:03:05 +1000

    saving

--- bleed-common/lib/SlotType.dart
+++ bleed-common/lib/SlotType.dart
@@ -3,6 +3,22 @@ enum SlotType {
   Pendant,
   Amulet,
   Brace,
+  Dagger,
+  Short_Sword,
+  Forest_Bow,
+  Wooden_Staff,
+  Leather_Cap,
+  Guards_Helmet
 }
+
+final List<SlotType> slotTypeWeapons = [
+  SlotType.Short_Sword,
+  SlotType.Forest_Bow,
+];
+
+final List<SlotType> slotTypeArmour = [
+  SlotType.Leather_Cap,
+  SlotType.Guards_Helmet,
+];
 
 final List<SlotType> slotTypes = SlotType.values;
