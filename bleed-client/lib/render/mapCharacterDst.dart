7d4c947492eac2af533eca08d7ab43865282a4fe
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-02-07 09:07:29 +1000

    saving

--- bleed-client/lib/render/mapCharacterDst.dart
+++ bleed-client/lib/render/mapCharacterDst.dart
@@ -3,18 +3,19 @@ import 'package:bleed_client/common/CharacterType.dart';
 import 'package:golden_ratio/constants.dart';
 import 'package:lemon_engine/engine.dart';
 
-const _scale = goldenRatio_0618;
 const _size = 64;
-const _scaledSize = _size * _scale;
-const _scaledSizeHalf = _scaledSize * 0.5;
-const _scaledThreeQuarters = _scaledSize * 0.75;
+const _anchorX = _size * 0.5;
+const _anchorY = _size * 0.75;
 
 void mapCharacterDst(
     Character character,
     CharacterType type,
     ) {
   return engine.actions.mapDst(
-      scale: _scale,
-      x: character.x - _scaledSizeHalf,
-      y: character.y - _scaledThreeQuarters);
+      scale: goldenRatio_0618,
+      x: character.x,
+      y: character.y,
+      anchorX: _anchorX,
+      anchorY: _anchorY,
+  );
 }
