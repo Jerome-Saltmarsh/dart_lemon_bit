7cd077301aa34becbc18629dfc8aaa5f8e9aec59
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-02-22 14:17:05 +1000

    saving

--- bleed-client/lib/modules/isometric/atlas.dart
+++ bleed-client/lib/modules/isometric/atlas.dart
@@ -12,16 +12,16 @@ const _pixelSize = 8.0;
 const _healthX = 2400.0;
 const _healthY = 0.0;
 const _healthBackgroundY = _healthY + _healthHeight;
-const _healthWidth = 52.0;
+const _healthWidth = 40.0;
 const _healthWidthHalf = _healthWidth * 0.5;
 const _healthHeight = 8.0;
 const _healthAnchorY = 50.0;
 
 void renderCharacterHealthBar(Character character){
-  engine.mapSrc(x: _healthX, y: _healthBackgroundY, width: _healthWidth, height: _healthHeight);
+  engine.mapSrc(x: _healthX, y: _healthBackgroundY, width: _healthWidth, height: 6);
   engine.mapDst(x: character.x, y: character.y, anchorX: _healthWidthHalf, anchorY: _healthAnchorY);
   engine.renderAtlas();
-  engine.mapSrc(x: _healthX, y: _healthY, width: _healthWidth * character.health, height: _healthHeight);
+  engine.mapSrc(x: _healthX, y: _healthY, width: _healthWidth * character.health, height: 6);
   engine.mapDst(x: character.x, y: character.y, anchorX: _healthWidthHalf, anchorY: _healthAnchorY);
   engine.renderAtlas();
 }
