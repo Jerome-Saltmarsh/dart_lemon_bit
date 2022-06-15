38510aaffb8d87521e334a311b622fa02fafaa7e
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-15 18:11:53 +0800

    v0.3.566

--- gamestream_flutter/lib/isometric/events/on_hour_changed.dart
+++ gamestream_flutter/lib/isometric/events/on_hour_changed.dart
@@ -1,7 +1,21 @@
 
-import 'package:gamestream_flutter/convert/convert_hour_to_ambient.dart';
+import 'package:bleed_common/library.dart';
 import 'package:gamestream_flutter/isometric/state/grid.dart';
 
-void onHourChanged(int hour){
-    ambient.value = convertHourToAmbient(hour);
+void onHourChanged(int hour) {
+    ambient.value = _convertHourToAmbient(hour);
+}
+
+int _convertHourToAmbient(int hour){
+    if (hour < 2) return Shade.Pitch_Black;
+    if (hour < 3) return Shade.Very_Dark;
+    if (hour < 4) return Shade.Dark;
+    if (hour < 6) return Shade.Medium;
+    if (hour < 9) return Shade.Bright;
+    if (hour < 15) return Shade.Very_Bright;
+    if (hour < 19) return Shade.Bright;
+    if (hour < 20) return Shade.Medium;
+    if (hour < 21) return Shade.Dark;
+    if (hour < 23) return Shade.Very_Dark;
+    return Shade.Pitch_Black;
 }
--- gamestream_flutter/lib/isometric/events/on_hour_changed_update_ambient.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:gamestream_flutter/convert/convert_hour_to_ambient.dart';
-import 'package:gamestream_flutter/isometric/state/grid.dart';
-
-void onHourChangedUpdateAmbient(int hour){
-    ambient.value = convertHourToAmbient(hour);
-}
