25288134801ea17e3098bddd6d6e2e3abf5e34be
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-19 17:50:00 +0800

    v0.3.641

--- gamestream_flutter/lib/isometric_web/on_mouse_left_clicked.dart
+++ gamestream_flutter/lib/isometric_web/on_mouse_left_clicked.dart
@@ -1,25 +1,18 @@
 
-import 'package:bleed_common/grid_node_type.dart';
 import 'package:gamestream_flutter/isometric/edit_state.dart';
-import 'package:gamestream_flutter/isometric/grid.dart';
 import 'package:gamestream_flutter/isometric/play_mode.dart';
-import 'package:gamestream_flutter/isometric/utils/convert.dart';
-import 'package:lemon_engine/engine.dart';
+import 'package:gamestream_flutter/isometric/utils/mouse_raycast.dart';
 
 void onMouseLeftClicked(){
   print("onMouseLeftClicked()");
 
   if (playModeEdit){
-    var row = convertWorldToRow(mouseWorldX, mouseWorldY, 24);
-    var column = convertWorldToColumn(mouseWorldX, mouseWorldY, 24);
-    for (var z = 0; z < gridTotalZ; z ++){
-      row++;
-      column ++;
-      if (grid[z][row][column] == GridNodeType.Empty) continue;
+    mouseRaycast((int z, int row, int column){
       edit.row = row;
       edit.column = column;
       edit.z = z;
-    }
+    });
     edit.refreshType();
   }
 }
+
