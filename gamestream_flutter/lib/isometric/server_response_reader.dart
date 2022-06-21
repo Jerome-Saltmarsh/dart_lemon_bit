1d3dcc1c83af096b9d3f4f0ed352574f2341c87e
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-21 09:16:24 +0800

    v0.3.657

--- gamestream_flutter/lib/isometric/server_response_reader.dart
+++ gamestream_flutter/lib/isometric/server_response_reader.dart
@@ -271,17 +271,17 @@ class ServerResponseReader with ByteReader {
               engine.cameraFollow(playerScreenX, playerScreenY, cameraFollowSpeed);
               final playerScreenX2 = player.renderX;
               final playerScreenY2 = player.renderY;
-              final distanceWorldX = ((playerScreenX2 - playerScreenX) / engine.zoom) * 0.5;
-              final distanceWorldY = ((playerScreenY2 - playerScreenY) / engine.zoom) * 0.5;
+              // final distanceWorldX = ((playerScreenX2 - playerScreenX) / engine.zoom) * 0.5;
+              // final distanceWorldY = ((playerScreenY2 - playerScreenY) / engine.zoom) * 0.5;
+              //
+              // engine.camera.x += distanceWorldX * 0.5;
+              // engine.camera.y += distanceWorldY * 0.5;
 
-              engine.camera.x += distanceWorldX * 0.5;
-              engine.camera.y += distanceWorldY * 0.5;
-
-              final distanceWorldX2 = ((playerScreenX2 - _previousPlayerScreenX2) / engine.zoom) * 0.5;
-              final distanceWorldY2 = ((playerScreenY2 - _previousPlayerScreenY2) / engine.zoom) * 0.5;
-
-              engine.camera.x += distanceWorldX2 * 0.4;
-              engine.camera.y += distanceWorldY2 * 0.4;
+              // final distanceWorldX2 = ((playerScreenX2 - _previousPlayerScreenX2) / engine.zoom) * 0.5;
+              // final distanceWorldY2 = ((playerScreenY2 - _previousPlayerScreenY2) / engine.zoom) * 0.5;
+              //
+              // engine.camera.x += distanceWorldX2 * 0.4;
+              // engine.camera.y += distanceWorldY2 * 0.4;
 
               final distanceWorldX3 = ((playerScreenX2 - _previousPlayerScreenX3) / engine.zoom) * 0.5;
               final distanceWorldY3 = ((playerScreenY2 - _previousPlayerScreenY3) / engine.zoom) * 0.5;
