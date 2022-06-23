32b94e5e34d954304623d727a708bf65c82f7dd3
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-23 10:51:41 +0800

    v0.3.691

--- bleed-server/bin/common/version.dart
+++ bleed-server/bin/common/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.3.690';
+const version = 'v0.3.691';
 
 
 
--- gamestream_flutter/lib/color_pitch_black.dart
+++ gamestream_flutter/lib/color_pitch_black.dart
@@ -5,9 +5,9 @@ final colorPitchBlack = Color.fromRGBO(37, 32, 48, 1.0);
 
 final colorShades = <int> [
   colorPitchBlack.withOpacity(0).value, // very bright
-  colorPitchBlack.withOpacity(0.2).value, // bright
-  colorPitchBlack.withOpacity(0.4).value, // medium
-  colorPitchBlack.withOpacity(0.6).value,  // dark
-  colorPitchBlack.withOpacity(0.8).value, // very dark
+  colorPitchBlack.withOpacity(0.35).value, // bright
+  colorPitchBlack.withOpacity(0.65).value, // medium
+  colorPitchBlack.withOpacity(0.85).value,  // dark
+  colorPitchBlack.withOpacity(0.95).value, // very dark
   colorPitchBlack.withOpacity(1.0).value, // pitchBlack
 ];
