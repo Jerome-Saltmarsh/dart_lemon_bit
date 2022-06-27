599308d14625758ae4f4bb22de0e757c53219b59
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-27 12:50:09 +0800

    v0.3.794

--- bleed-server/bin/common/version.dart
+++ bleed-server/bin/common/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.3.793';
+const version = 'v0.3.794';
 
 
 
--- gamestream_flutter/lib/isometric/grid/actions/rain_off.dart
+++ gamestream_flutter/lib/isometric/grid/actions/rain_off.dart
@@ -4,13 +4,13 @@ import 'package:bleed_common/grid_node_type.dart';
 import 'package:gamestream_flutter/isometric/grid.dart';
 
 void apiGridActionRainOff(){
-  for (var z = gridTotalZ - 1; z >= 0; z--) {
-    for (var row = 0; row < gridTotalRows; row++) {
-      for (var column = 0; column < gridTotalColumns; column++) {
-        final type = grid[z][row][column];
-        if (!GridNodeType.isRain(type)) continue;
-        grid[z][row][column] = GridNodeType.Empty;
-      }
-    }
-  }
+  gridForEach(
+      where: GridNodeType.isRain,
+      apply: assignEmpty,
+  );
+}
+
+
+void assignEmpty(int z, int row, int column, int type){
+  grid[z][row][column] = GridNodeType.Empty;
 }
