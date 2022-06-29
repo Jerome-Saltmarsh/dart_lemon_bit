3953e1f9e5609c2a9c0cfc15496bda1c7d1cad54
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-29 18:32:59 +0800

    v0.3.830

--- gamestream_flutter/lib/isometric/weather/lightning.dart
+++ gamestream_flutter/lib/isometric/weather/lightning.dart
@@ -5,7 +5,11 @@ import 'package:lemon_math/library.dart';
 import 'package:gamestream_flutter/isometric/game_action.dart';
 import 'package:lemon_watch/watch.dart';
 
-final weatherLightning = Watch(false);
+final weatherLightning = Watch(false, onChanged: (bool lightningOn){
+   if (lightningOn){
+     nextLightning = 0;
+   }
+});
 var nextLightning = 0;
 
 void weatherLightningToggle() => weatherLightning.value = !weatherLightning.value;
