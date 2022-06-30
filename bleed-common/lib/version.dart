268e3d5ce9200b98eac7be73a0221d3f130c5685
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-06-30 10:25:57 +0800

    v0.3.841

--- bleed-common/lib/version.dart
+++ bleed-common/lib/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.3.840';
+const version = 'v0.3.841';
 
 
 
--- gamestream_flutter/lib/isometric/audio/audio_single.dart
+++ gamestream_flutter/lib/isometric/audio/audio_single.dart
@@ -4,11 +4,12 @@ import 'package:just_audio/just_audio.dart';
 class AudioSingle {
   final String name;
   late AudioSource source;
-  final player = AudioPlayer();
+  final players = <AudioPlayer>[];
+
+  String get url => 'assets/audio/sounds/$name.mp3';
 
   AudioSingle(this.name){
-    source = AudioSource.uri(Uri.parse('assets/audio/sounds/$name.mp3'));
-    player.setAudioSource(source);
+    source = AudioSource.uri(Uri.parse(url));
   }
 
   void call(double volume){
@@ -16,9 +17,23 @@ class AudioSingle {
   }
 
   void play({double volume = 1.0}) async {
+    final player = getAudioPlayer;
+    assert (!player.playing);
     await player.setVolume(volume);
     if (player.audioSource == null) throw Exception("no audio source");
     await player.seek(const Duration());
     await player.play();
+    await player.seek(const Duration());
+  }
+
+  AudioPlayer get getAudioPlayer {
+     for (final player in players) {
+       if (player.playing) continue;
+       return player;
+     }
+     final newPlayer = AudioPlayer();
+     newPlayer.setAudioSource(source);
+     players.add(newPlayer);
+     return newPlayer;
   }
 }
