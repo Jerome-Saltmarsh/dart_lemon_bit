af0a3a3731c2010cd99ea219d041f1813e3886cb
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-01 15:25:51 +0800

    v0.3.873

--- bleed-server/bin/common/version.dart
+++ bleed-server/bin/common/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.3.872';
+const version = 'v0.3.873';
 
 
 
--- gamestream_flutter/lib/isometric/events/on_rain_changed.dart
+++ gamestream_flutter/lib/isometric/events/on_rain_changed.dart
@@ -2,6 +2,5 @@ import 'package:gamestream_flutter/isometric/grid/actions/rain_off.dart';
 import 'package:gamestream_flutter/isometric/grid/actions/rain_on.dart';
 
 void onRainChanged(bool raining) {
-  print("on rain changed $raining");
   raining ? apiGridActionRainOn() : apiGridActionRainOff();
 }
