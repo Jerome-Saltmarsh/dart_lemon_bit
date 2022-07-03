dd67761715f928971731712b515a43c9eb334d37
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-04 07:31:50 +0800

    v0.3.934

--- gamestream_flutter/lib/isometric/grid/actions/rain_on.dart
+++ gamestream_flutter/lib/isometric/grid/actions/rain_on.dart
@@ -8,15 +8,17 @@ void apiGridActionRainOn(){
       for (var z = gridTotalZ - 1; z >= 0; z--) {
         final type = grid[z][row][column];
         if (!isEmpty(type)) {
+          if (type == GridNodeType.Torch) break;
+          if (type == GridNodeType.Tree_Top) break;
           setGridType(z + 1, row, column, GridNodeType.Rain_Landing);
           setGridType(z + 2, row, column, GridNodeType.Rain_Falling);
           break;
         } else {
           if (column == 0 || !isEmpty(grid[z][row][column - 1])){
-            grid[z][row][column] = GridNodeType.Rain_Falling;
+            setGridType(z, row, column, GridNodeType.Rain_Falling);
           } else
           if (row == 0 || !isEmpty(grid[z][row - 1][column])){
-            grid[z][row][column] = GridNodeType.Rain_Falling;
+            setGridType(z, row, column, GridNodeType.Rain_Falling);
           }
         }
       }
