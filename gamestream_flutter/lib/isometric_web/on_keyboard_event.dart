43ee35ddaa47e63510efef8e0b78a8bbacef1b13
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-06 05:49:45 +0800

    v0.3.979

--- gamestream_flutter/lib/isometric_web/on_keyboard_event.dart
+++ gamestream_flutter/lib/isometric_web/on_keyboard_event.dart
@@ -8,49 +8,55 @@ import 'package:lemon_engine/engine.dart';
 
 void onKeyboardEvent(RawKeyEvent event){
   if (event is RawKeyDownEvent){
-    if (event.physicalKey == PhysicalKeyboardKey.space){
-      lowerTileMode = !lowerTileMode;
-    }
-    if (event.physicalKey == PhysicalKeyboardKey.arrowUp){
-      if (keyPressed(LogicalKeyboardKey.shiftLeft)){
-        edit.z.value++;
-        if (edit.z.value >= gridTotalZ) {
-          edit.z.value = gridTotalZ - 1;
-        }
-      } else {
-        edit.row.value--;
-        if (edit.row.value < 0){
-          edit.row.value = 0;
-        }
-      }
-    }
-    if (event.physicalKey == PhysicalKeyboardKey.arrowRight){
-      edit.column.value--;
-      if (edit.column.value < 0){
-        edit.column.value = 0;
-      }
-    }
-    if (event.physicalKey == PhysicalKeyboardKey.arrowDown){
-      if (keyPressed(LogicalKeyboardKey.shiftLeft)){
-        edit.z.value--;
-        if (edit.z.value < 0){
-          edit.z.value = 0;
-        }
-      } else {
-        edit.row.value = min(edit.row.value + 1, gridTotalRows - 1);
-      }
-    }
-    if (event.physicalKey == PhysicalKeyboardKey.arrowLeft){
-      edit.column.value++;
-      if (edit.column.value >= gridTotalColumns){
-        edit.column.value = gridTotalColumns - 1;
-      }
-    }
-    edit.type.value = grid[edit.z.value][edit.row.value][edit.column.value];
-    return;
+    return onRawKeyDownEvent(event);
   }
   if (event is RawKeyUpEvent){
     return;
   }
+}
+
+void onRawKeyDownEvent(RawKeyDownEvent event){
+  final key = event.physicalKey;
+  if (key == PhysicalKeyboardKey.space){
+    lowerTileMode = !lowerTileMode;
+  }
+  if (key == PhysicalKeyboardKey.arrowUp){
+    if (keyPressed(LogicalKeyboardKey.shiftLeft)){
+      edit.z.value++;
+      if (edit.z.value >= gridTotalZ) {
+        edit.z.value = gridTotalZ - 1;
+      }
+    } else {
+      edit.row.value--;
+      if (edit.row.value < 0){
+        edit.row.value = 0;
+      }
+    }
+  }
+  if (key == PhysicalKeyboardKey.arrowRight){
+    edit.column.value--;
+    if (edit.column.value < 0){
+      edit.column.value = 0;
+    }
+  }
+  if (key == PhysicalKeyboardKey.arrowDown){
+    if (keyPressed(LogicalKeyboardKey.shiftLeft)){
+      edit.z.value--;
+      if (edit.z.value < 0){
+        edit.z.value = 0;
+      }
+    } else {
+      edit.row.value = min(edit.row.value + 1, gridTotalRows - 1);
+    }
+  }
+  if (key == PhysicalKeyboardKey.arrowLeft){
+    edit.column.value++;
+    if (edit.column.value >= gridTotalColumns){
+      edit.column.value = gridTotalColumns - 1;
+    }
+  }
+  edit.type.value = grid[edit.z.value][edit.row.value][edit.column.value];
+  return;
+
 }
 
