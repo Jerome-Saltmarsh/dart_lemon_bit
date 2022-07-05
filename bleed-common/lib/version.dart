7cf5f4d724d8acb0f9d8bee91339a655e2926ba6
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-05 17:44:05 +0800

    v0.3.972

--- bleed-common/lib/version.dart
+++ bleed-common/lib/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.3.971';
+const version = 'v0.3.972';
 
 
 
--- gamestream_flutter/lib/isometric/events/on_changed_scene.dart
+++ gamestream_flutter/lib/isometric/events/on_changed_scene.dart
@@ -2,8 +2,8 @@
 import 'package:gamestream_flutter/isometric/particles.dart';
 
 void onChangedScene(){
-  for (var i = 0; i < totalParticles; i++){
-     particles[i].duration = 0;
+  for (final particle in particles){
+    particle.duration = 0;
   }
   totalParticles = 0;
 }
