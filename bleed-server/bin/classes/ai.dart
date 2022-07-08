50d33c05ad4298e163cdef8301cd2c2e7983b6f0
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-08 10:45:48 +0800

    v0.4.032

--- bleed-server/bin/classes/ai.dart
+++ bleed-server/bin/classes/ai.dart
@@ -60,7 +60,42 @@ class AI extends Character with Material {
 
   @override
   void customUpdateCharacter(Game game){
+    if (deadOrBusy) return;
+
+    final target = this.target;
+    if (target != null) {
+      if (withinAttackRange(target)) {
+        attackTarget(target);
+        return;
+      }
+      const runAtTargetDistance = 100;
+      if ((getDistance(target) < runAtTargetDistance)) {
+        return runAt(target);
+      }
+    }
+
+    if (!arrivedAtDest){
+      faceDestination();
+      setCharacterStateRunning();
+      return;
+    }
+
+    if (pathIndex > 0){
+      pathIndex--;
+      destX = pathX[pathIndex];
+      destY = pathY[pathIndex];
+      faceDestination();
+      setCharacterStateRunning();
+      return;
+    }
+    state = CharacterState.Idle;
+
     customUpdateAI(game);
+  }
+
+  void faceDestination() {
+    if (deadOrBusy) return;
+    angle = getAngleBetween(x, y, destX, destY);
   }
 
   void customUpdateAI(Game game){
--- bleed-server/bin/classes/zombie.dart
+++ bleed-server/bin/classes/zombie.dart
@@ -24,40 +24,6 @@ class Zombie extends AI {
 
   @override
   void customUpdateAI(Game game) {
-    if (deadOrBusy) return;
-
-    final target = this.target;
-    if (target != null) {
-        if (withinAttackRange(target)) {
-          attackTarget(target);
-          return;
-        }
-        const runAtTargetDistance = 100;
-        if ((getDistance(target) < runAtTargetDistance)) {
-          return runAt(target);
-        }
-    }
-
     applyBehaviorWander();
-
-    if (!arrivedAtDest){
-      faceDestination();
-      setCharacterStateRunning();
-      return;
-    }
-    if (pathIndex > 0){
-       pathIndex--;
-       destX = pathX[pathIndex];
-       destY = pathY[pathIndex];
-       faceDestination();
-       setCharacterStateRunning();
-       return;
-    }
-    state = CharacterState.Idle;
-  }
-
-  void faceDestination() {
-    if (deadOrBusy) return;
-    angle = getAngleBetween(x, y, destX, destY);
   }
 }
