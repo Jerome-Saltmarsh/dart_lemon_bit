b9992f8ead311e29b4eca0642dafa73c5f136c19
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-10 10:48:43 +0800

    v0.4.047

--- bleed-server/bin/common/version.dart
+++ bleed-server/bin/common/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.4.046';
+const version = 'v0.4.047';
 
 
 
--- gamestream_flutter/lib/isometric/render/render_grid_node.dart
+++ gamestream_flutter/lib/isometric/render/render_grid_node.dart
@@ -6,162 +6,69 @@ import 'package:gamestream_flutter/isometric/watches/torches_ignited.dart';
 import 'package:lemon_engine/actions/render_atlas.dart';
 import 'package:lemon_engine/render.dart';
 
+import '../grid.dart';
 import '../grid/state/wind.dart';
 import '../variables/src_x_rain_falling.dart';
 import '../variables/src_x_rain_landing.dart';
 import 'render_torch.dart';
 
+
+var _dstX = 0.0;
+var _dstY = 0.0;
+var _shade = 0;
+
 void renderGridNode(int z, int row, int column, int type, double dstY, int shade) {
-  final dstX = (row - column) * tileSizeHalf;
+  _dstX = (row - column) * tileSizeHalf;
+  _dstY = dstY;
+  // _shade = gridLightDynamic[z][row][column];
+  _shade = shade;
+
   switch (type) {
 
     case GridNodeType.Bricks:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7110,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7110);
     case GridNodeType.Grass:
       final wind = gridWind[z][row][column];
       switch(wind){
         case windIndexCalm:
-          return render(
-            dstX: dstX,
-            dstY: dstY,
-            srcX: 5267,
-            srcY: 72.0 * shade,
-            srcWidth: 48,
-            srcHeight: 72,
-            anchorX: 0.5,
-            anchorY: 0.3334,
-          );
+          return renderBlockSrcX(5267);
         default:
-          return render(
-            dstX: dstX,
-            dstY: dstY,
-            srcX: 5267 + ((((row - column) + animationFrameGrassShort) % 6) * 48),
-            srcY: 72.0 * shade,
-            srcWidth: 48,
-            srcHeight: 72,
-            anchorX: 0.5,
-            anchorY: 0.3334,
-          );
+          return renderBlockSrcX(5267 + ((((row - column) + animationFrameGrassShort) % 6) * 48));
       }
 
     case GridNodeType.Grass_Slope_West:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7781 ,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorX: 0.5,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7781);
 
     case GridNodeType.Grass_Slope_South:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7829 ,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorX: 0.5,
-        anchorY: 0.3334,
-      );
-    case GridNodeType.Grass_Slope_East:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7877 ,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorX: 0.5,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7829);
+
+      case GridNodeType.Grass_Slope_East:
+        return renderBlockSrcX(7877);
+
     case GridNodeType.Grass_Slope_North:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7925 ,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorX: 0.5,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7925);
 
     case GridNodeType.Wood:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7590 ,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorX: 0.5,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7590);
+
     case GridNodeType.Grass_Long:
       final wind = gridWind[z][row][column];
       switch (wind){
         case windIndexCalm:
-          return render(
-            dstX: dstX,
-            dstY: dstY,
-            srcX: 4856,
-            srcY: 72.0 * shade,
-            srcWidth: 48,
-            srcHeight: 72,
-            anchorX: 0.5,
-            anchorY: 0.3334,
-          );
+          return renderBlockSrcX(4856);
         case windIndexGentle:
-          return render(
-            dstX: dstX,
-            dstY: dstY,
-            srcX: 4856 + ((((row - column) + animationFrameGrass) % 4) * 48),
-            srcY: 72.0 * shade,
-            srcWidth: 48,
-            srcHeight: 72,
-            anchorX: 0.5,
-            anchorY: 0.3334,
-          );
+          return renderBlockSrcX(4856 + ((((row - column) + animationFrameGrass) % 4) * 48));
          default:
-          return render(
-            dstX: dstX,
-            dstY: dstY,
-            srcX: 5048 + ((((row - column) + animationFrameGrass) % 4) * 48),
-            srcY: 72.0 * shade,
-            srcWidth: 48,
-            srcHeight: 72,
-            anchorX: 0.5,
-            anchorY: 0.3334,
-          );
+           return renderBlockSrcX(5048 + ((((row - column) + animationFrameGrass) % 4) * 48));
       }
 
     case GridNodeType.Rain_Falling:
-      return render(
-        dstX: dstX - rainPosition,
-        dstY: dstY + animationFrameRain,
-        srcX: srcXRainFalling,
-        srcY: 72.0 * animationFrameRain,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-        color: colorShades[shade],
-      );
+      _dstY += animationFrameRain;
+      return renderBlockSrcX(srcXRainFalling);
 
     case GridNodeType.Rain_Landing:
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY,
         srcX: srcXRainLanding,
         srcY: 72.0 * animationFrameRain,
@@ -171,64 +78,24 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
         color: colorShades[shade],
       );
     case GridNodeType.Stairs_South:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7398,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7398);
     case GridNodeType.Stairs_West:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7446,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7446);
     case GridNodeType.Stairs_North:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7494,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7494);
     case GridNodeType.Stairs_East:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 7542,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(7542);
     case GridNodeType.Brick_Top:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 8621,
-        srcY: 72.0 * shade,
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+      return renderBlockSrcX(8621);
     case GridNodeType.Wood_Half_Row_1:
-      return renderBlock(dstX: dstX, dstY: dstY, srcX: 8887, shade: shade);
+      return renderBlockSrcX(8887);
     case GridNodeType.Wood_Half_Column_1:
-      return renderBlock(dstX: dstX, dstY: dstY, srcX: 8935, shade: shade);
+      return renderBlockSrcX(8935);
     case GridNodeType.Wood_Corner_Bottom:
-      return renderBlock(dstX: dstX, dstY: dstY, srcX: 8983, shade: shade);
+      return renderBlockSrcX(8983);
     case GridNodeType.Water:
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY + animationFrameWaterHeight,
         srcX: 7206 + animationFrameWaterSrcX,
         srcY: 72.0 * shade,
@@ -238,7 +105,7 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
       );
     case GridNodeType.Water_Flowing:
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY + animationFrameWaterHeight,
         srcX: 8096 + animationFrameWaterSrcX,
         srcY: 72.0 * shade,
@@ -248,18 +115,18 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
       );
     case GridNodeType.Torch:
       if (!torchesIgnited.value) {
-        return renderTorchOff(dstX, dstY);
+        return renderTorchOff(_dstX, dstY);
       }
       final wind = gridWind[z][row][column];
 
       if (wind == Wind.Calm){
-        return renderTorchOn(dstX, dstY);
+        return renderTorchOn(_dstX, dstY);
       }
-      return renderTorchOnWindy(dstX, dstY);
+      return renderTorchOnWindy(_dstX, dstY);
 
     case GridNodeType.Tree_Bottom:
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY,
         srcX: 1478,
         srcY: 68.0 * shade,
@@ -271,7 +138,7 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
       final wind = gridWind[z][row][column];
       animationFrameTreePosition = treeAnimation[(row - column + animationFrame) % treeAnimation.length] * wind;
       return render(
-        dstX: dstX + (animationFrameTreePosition * 0.5),
+        dstX: _dstX + (animationFrameTreePosition * 0.5),
         dstY: dstY,
         srcX: 1478 + 62,
         srcY: 68.0 * shade,
@@ -280,9 +147,19 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
         anchorY: 0.33,
       );
 
+    case GridNodeType.Fireplace:
+      return render(
+        dstX: _dstX,
+        dstY: dstY,
+        srcX: 6469,
+        srcY: (((row + column + (animationFrameTorch)) % 6) * 72),
+        srcWidth: 48,
+        srcHeight: 72,
+        anchorY: 0.3334,
+      );
     case GridNodeType.Player_Spawn:
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY,
         srcX: 7686,
         srcY: 72.0 * shade,
@@ -292,7 +169,7 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
       );
     case GridNodeType.Wooden_Wall_Row:
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY,
         srcX: 7782,
         srcY: 72.0 * shade,
@@ -304,7 +181,7 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
     case GridNodeType.Enemy_Spawn:
       if (!playModeEdit) return;
       return render(
-        dstX: dstX,
+        dstX: _dstX,
         dstY: dstY,
         srcX: 7686,
         srcY: 72.0 * shade,
@@ -312,49 +189,39 @@ void renderGridNode(int z, int row, int column, int type, double dstY, int shade
         srcHeight: 72,
         anchorY: 0.3334,
       );
-    case GridNodeType.Fireplace:
-      return render(
-        dstX: dstX,
-        dstY: dstY,
-        srcX: 6469,
-        srcY: (((row + column + (animationFrameTorch)) % 6) * 72),
-        srcWidth: 48,
-        srcHeight: 72,
-        anchorY: 0.3334,
-      );
+
     default:
       throw Exception("Cannot render grid node type $type");
   }
 }
 
-void renderBlock({
-  required double dstX,
-  required double dstY,
-  required double srcX,
-  required int shade
-}){
+void renderBlockSrcX(double srcX){
 
   const spriteWidth = 48.0;
   const spriteHeight = 72.0;
   const spriteWidthHalf = 24.0;
   const spriteHeightHalf = 36.0;
 
-  final srcY = shade * spriteHeight;
+  final srcY = _shade * spriteHeight;
 
   src[bufferIndex] = srcX;
   dst[bufferIndex] = 1;
+  colors[renderIndex] = 0;
+
   bufferIndex++;
 
-  src[bufferIndex] = srcY;
+  src[bufferIndex] = srcY; // srcY
   dst[bufferIndex] = 0;
+
   bufferIndex++;
 
   src[bufferIndex] = srcX + spriteWidth;
-  dst[bufferIndex] = dstX - spriteWidthHalf;
+  dst[bufferIndex] = _dstX - spriteWidthHalf;
 
   bufferIndex++;
+
   src[bufferIndex] = srcY + spriteHeight;
-  dst[bufferIndex] = dstY - spriteHeightHalf;
+  dst[bufferIndex] = _dstY - spriteHeightHalf;
 
   bufferIndex++;
   renderIndex++;
