1260b9fce89b9f6a5cfc28a7de74bf1f6ee823af
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-12 09:30:37 +0800

    v0.4.090

--- bleed-common/lib/version.dart
+++ bleed-common/lib/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.4.089';
+const version = 'v0.4.090';
 
 
 
--- gamestream_flutter/lib/isometric/ui/controls/build_control_npc_talk.dart
+++ gamestream_flutter/lib/isometric/ui/controls/build_control_npc_talk.dart
@@ -5,11 +5,12 @@ import 'package:gamestream_flutter/isometric/ui/widgets/build_container.dart';
 import 'package:gamestream_flutter/network/send_client_request.dart';
 import 'package:gamestream_flutter/utils/string_utils.dart';
 
+import '../../../flutterkit.dart';
 import '../widgets/nothing.dart';
 
 Widget buildControlNpcTalk(String? value) =>
     isNullOrEmpty(value) ? nothing : container(
-        child: value,
+        child: text(value, color: white80),
         color: brownLight,
         width: 300,
         height: 200,
@@ -21,7 +22,7 @@ Widget buildControlNpcTopics(List<String> topics) =>
     children: topics.map((String value) {
       return container(
           margin: const EdgeInsets.only(top: 6),
-          child: value,
+          child: text(value, color: white80),
           color: brownLight,
           hoverColor: brownDark,
           action: () {
