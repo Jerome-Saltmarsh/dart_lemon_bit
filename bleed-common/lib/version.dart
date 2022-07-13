25071cc68d9121c0710fdd7eefb3d22d512d6fcd
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-13 12:01:08 +0800

    v0.4.110

--- bleed-common/lib/version.dart
+++ bleed-common/lib/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.4.109';
+const version = 'v0.4.110';
 
 
 
--- gamestream_flutter/lib/isometric/events/on_player_event_quest_completed.dart
+++ gamestream_flutter/lib/isometric/events/on_player_event_quest_completed.dart
@@ -1,4 +1,6 @@
 
-void onPlayerEventQuestCompleted(){
+import 'package:gamestream_flutter/isometric/audio/audio_singles.dart';
 
+void onPlayerEventQuestCompleted(){
+  audioSingleNotificationSound12();
 }
