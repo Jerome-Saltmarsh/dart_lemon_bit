14084c4a70ea00d8cd8d0d36493b7e7fc82b88f8
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-15 08:46:09 +0800

    v0.4.142

--- bleed-common/lib/version.dart
+++ bleed-common/lib/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.4.41';
+const version = 'v0.4.142';
 
 
 
--- gamestream_flutter/lib/isometric/events/on_changed_map_x.dart
+++ gamestream_flutter/lib/isometric/events/on_changed_map_x.dart
@@ -6,6 +6,8 @@ void onChangedMapX(int value){
    cameraCenterOnPlayer();
    // engine.camera.x -= 100;
    // engine.camera.y -= 100;
+
+
 }
 
 void onChangedMapY(int value){
