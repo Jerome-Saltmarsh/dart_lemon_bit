5097c1e363ea2d1605e90a8e3a3916e9e62f39bb
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-07-29 06:51:01 +0800

    v0.4.319

--- bleed-server/bin/dark_age/game_dark_age.dart
+++ bleed-server/bin/dark_age/game_dark_age.dart
@@ -20,6 +20,7 @@ class GameDarkAge extends Game {
   @override
   void setHourMinutes(int hour, int minutes){
     environment.time.time = (hour * secondsPerHour) + (minutes * secondsPerMinute);
+    environment.updateShade();
     playersWriteWeather();
   }
 
