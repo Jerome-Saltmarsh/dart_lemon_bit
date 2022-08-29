b9a756c955ff075341586b62c05b94138e77d50c
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-08-29 10:55:54 +0800

    v0.4.708

--- bleed-server/bin/common/version.dart
+++ bleed-server/bin/common/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.4.707';
+const version = 'v0.4.708';
 
 
 
--- gamestream_flutter/lib/isometric/play_mode.dart
+++ gamestream_flutter/lib/isometric/play_mode.dart
@@ -1,6 +1,8 @@
 import 'package:gamestream_flutter/isometric/events/on_changed_mode.dart';
 import 'package:lemon_watch/watch.dart';
 
+import 'watches/scene_meta_data.dart';
+
 final playMode = Watch(Mode.Play, onChanged: onChangedMode);
 
 bool get playModeEdit => playMode.value == Mode.Edit;
@@ -10,6 +12,7 @@ void setPlayModePlay() => playMode.value = Mode.Play;
 void setPlayModeEdit() => playMode.value = Mode.Edit;
 
 void actionPlayModeToggle(){
+    if (!sceneMetaDataMapEditable.value) return;
     modeIsPlay ? setPlayModeEdit() : setPlayModePlay();
 }
 
