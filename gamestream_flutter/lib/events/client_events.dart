d2591276e27a63701de582d41de7bc575a007659
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-11-14 05:29:19 +0800

    v0.6.079

--- gamestream_flutter/lib/events/client_events.dart
+++ gamestream_flutter/lib/events/client_events.dart
@@ -44,6 +44,10 @@ class ClientEvents {
         if (ClientState.hoverIndex.value >= 0 && ClientState.hoverDialogIsInventory){
           ClientState.hotKey1.value = ServerState.inventory[ClientState.hoverIndex.value];
         } else {
+          if (ClientState.hotKey1.value == ItemType.Empty) {
+            ClientState.hotKey1.value = GamePlayer.weapon.value;
+            return;
+          }
           ServerActions.equipItemType(ClientState.hotKey1.value);
         }
         return;
@@ -52,6 +56,10 @@ class ClientEvents {
         if (ClientState.hoverIndex.value >= 0 && ClientState.hoverDialogIsInventory){
           ClientState.hotKey2.value = ServerState.inventory[ClientState.hoverIndex.value];
         } else {
+          if (ClientState.hotKey2.value == ItemType.Empty) {
+            ClientState.hotKey2.value = GamePlayer.weapon.value;
+            return;
+          }
           ServerActions.equipItemType(ClientState.hotKey2.value);
         }
         return;
@@ -60,14 +68,35 @@ class ClientEvents {
         if (ClientState.hoverIndex.value >= 0 && ClientState.hoverDialogIsInventory){
           ClientState.hotKey3.value = ServerState.inventory[ClientState.hoverIndex.value];
         } else {
+          if (ClientState.hotKey3.value == ItemType.Empty) {
+            ClientState.hotKey3.value = GamePlayer.weapon.value;
+            return;
+          }
           ServerActions.equipItemType(ClientState.hotKey3.value);
         }
         return;
       }
+      if (key == LogicalKeyboardKey.digit4) {
+        if (ClientState.hoverIndex.value >= 0 && ClientState.hoverDialogIsInventory){
+          ClientState.hotKey4.value = ServerState.inventory[ClientState.hoverIndex.value];
+        } else {
+          if (ClientState.hotKey4.value == ItemType.Empty) {
+            ClientState.hotKey4.value = GamePlayer.weapon.value;
+            return;
+          }
+          ServerActions.equipItemType(ClientState.hotKey4.value);
+        }
+        return;
+      }
+
       if (key == LogicalKeyboardKey.keyQ) {
         if (ClientState.hoverIndex.value >= 0 && ClientState.hoverDialogIsInventory){
           ClientState.hotKeyQ.value = ServerState.inventory[ClientState.hoverIndex.value];
         } else {
+          if (ClientState.hotKeyQ.value == ItemType.Empty) {
+            ClientState.hotKeyQ.value = GamePlayer.weapon.value;
+            return;
+          }
           ServerActions.equipItemType(ClientState.hotKeyQ.value);
         }
         return;
@@ -76,6 +105,10 @@ class ClientEvents {
         if (ClientState.hoverIndex.value >= 0 && ClientState.hoverDialogIsInventory){
           ClientState.hotKeyE.value = ServerState.inventory[ClientState.hoverIndex.value];
         } else {
+          if (ClientState.hotKeyE.value == ItemType.Empty) {
+            ClientState.hotKeyE.value = GamePlayer.weapon.value;
+            return;
+          }
           ServerActions.equipItemType(ClientState.hotKeyE.value);
         }
         return;
