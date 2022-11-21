06869fb2c2a8ad0ffc89852a80ed0964f13171c4
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-11-21 08:47:14 +0800

    v0.6.218

--- gamestream_flutter/lib/src/server/src/server_query.dart
+++ gamestream_flutter/lib/src/server/src/server_query.dart
@@ -24,19 +24,10 @@ class ServerQuery {
     throw Exception('ServerQuery.getWatchBeltQuantity($watchBelt)');
   }
 
-  static int getItemQuantity(int itemType) {
-     var total = 0;
-     for (var i = 0; i < ServerState.inventory.length; i++){
-        if (ServerState.inventory[i] != itemType) continue;
-        total += ServerState.inventoryQuantity[i];
-     }
-     return total;
-  }
-
   static int getItemTypeConsumesRemaining(int itemType) {
     final consumeAmount = ItemType.getConsumeAmount(itemType);
     if (consumeAmount <= 0) return 0;
-    return getItemQuantity(ItemType.getConsumeType(itemType)) ~/ consumeAmount;
+    return countItemTypeQuantityInPlayerPossession(ItemType.getConsumeType(itemType)) ~/ consumeAmount;
   }
 
   static int mapWatchBeltTypeToItemType(Watch<int> watchBeltType){
