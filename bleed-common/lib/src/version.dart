25706f921b2838b044e6df5e7b6b3a05c322c007
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-11-22 10:01:54 +0800

    v0.6.242

--- bleed-common/lib/src/version.dart
+++ bleed-common/lib/src/version.dart
@@ -1,1 +1,1 @@
-const version = 'v0.6.241';
+const version = 'v0.6.242';
--- gamestream_flutter/lib/language_utils.dart
+++ gamestream_flutter/lib/language_utils.dart
@@ -24,7 +24,7 @@ String formatPercentage(num a) => '${(a * 100).toInt()}%';
 
 double getPercentageDifference(num a, num b) {
   if (a == 0 && b == 0) return 0;
-  if (a == 0) return 1.0;
-  if (b == 0) return -1;
+  if (a == 0) return -1.0;
+  if (b == 0) return 1.0;
   return (a - b) / b;
 }
