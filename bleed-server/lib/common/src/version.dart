252568352bd2c3af28b3042bd028f90b8c2c738f
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-12-03 08:42:30 +0800

    v0.6.395

--- bleed-server/lib/common/src/version.dart
+++ bleed-server/lib/common/src/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.6.394';
+const version = 'v0.6.395';
 
 
 
--- bleed-server/lib/src/classes/src/ai.dart
+++ bleed-server/lib/src/classes/src/ai.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:bleed_server/src/maths/get_distance_between_v3.dart';
 import 'package:lemon_math/library.dart';
 
 import 'package:bleed_server/gamestream.dart';
@@ -7,6 +9,7 @@ import 'package:bleed_server/gamestream.dart';
 class AI extends Character {
   static const AI_Path_Size = 80;
   static const Destination_Radius = 15;
+  static const Frames_Between_AI_Mode = 120;
 
   final pathX = Uint16List(AI_Path_Size);
   final pathY = Uint16List(AI_Path_Size);
@@ -24,6 +27,7 @@ class AI extends Character {
   var wanderRadius = 0.0;
   var nextTeleport = randomInt(500, 1000);
   var aiMode = AIMode.Idle;
+  var aiModeNext = Frames_Between_AI_Mode;
   Function(Player player)? onInteractedWith;
 
   AI({
@@ -99,18 +103,60 @@ class AI extends Character {
   bool withinChaseRange(Position3 target) {
     return withinRadius(target, chaseRange);
   }
+
+  void shuffleAIMode(){
+    aiMode = randomInt(0, 5);
+  }
   
   void updateAI(){
     if (busy) return;
 
+    aiModeNext--;
+    if (aiModeNext <= 0){
+      shuffleAIMode();
+      aiModeNext = Frames_Between_AI_Mode;
+    }
+
+    final target = this.target;
     if (target != null) {
-      if (withinAttackRange(target!)) {
-        face(target!);
+
+      if (withinAttackRange(target) && aiMode != AIMode.Idle) {
         setCharacterStatePerforming(duration: equippedAttackDuration);
         return;
       }
-      destX = target!.x;
-      destY = target!.y;
+
+      switch (aiMode) {
+        case AIMode.Idle:
+          setCharacterStateIdle();
+          return;
+        case AIMode.Pursue:
+          face(target);
+          if (withinAttackRange(target)) {
+            setCharacterStatePerforming(duration: equippedAttackDuration);
+            return;
+          } else {
+            destX = target.x;
+            destY = target.y;
+          }
+          break;
+        case AIMode.Evade:
+          face(target);
+          faceAngle += pi;
+          setCharacterStateRunning();
+          return;
+        case AIMode.Encircle_CW:
+          final targetAngle = getAngleBetween(target.x, target.y, x, y) + piEighth;
+          final distance = getDistanceBetweenV3(this, target);
+          destX = x + getAdjacent(targetAngle, distance);
+          destY = y + getOpposite(targetAngle, distance);
+          break;
+        case AIMode.Encircle_CCW:
+          final targetAngle = getAngleBetween(target.x, target.y, x, y) - piEighth;
+          final distance = getDistanceBetweenV3(this, target);
+          destX = x + getAdjacent(targetAngle, distance);
+          destY = y + getOpposite(targetAngle, distance);
+          break;
+      }
     }
 
     if (!arrivedAtDest) {
@@ -128,6 +174,10 @@ class AI extends Character {
       return;
     }
     state = CharacterState.Idle;
-    // applyBehaviorWander(this);
+  }
+
+  double get getAngleBetweenTarget {
+    assert (target != null);
+    return getAngleBetween(x, y, target!.x, target!.y);
   }
 }
