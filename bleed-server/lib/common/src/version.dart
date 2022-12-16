37a72bbbcebe0682c2464e27d791a6784b20ec13
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2022-12-17 05:33:17 +0800

    v0.6.579

--- bleed-server/lib/common/src/version.dart
+++ bleed-server/lib/common/src/version.dart
@@ -1,4 +1,4 @@
-const version = 'v0.6.578';
+const version = 'v0.6.579';
 
 
 
--- gamestream_flutter/lib/game_lighting.dart
+++ gamestream_flutter/lib/game_lighting.dart
@@ -4,28 +4,34 @@ import 'package:gamestream_flutter/library.dart';
 class GameLighting {
   static final Default_Color_Start =  Color.fromRGBO(38, 34, 47, 1.0).withOpacity(0);
   static final Default_Color_End =    Color.fromRGBO(47, 34, 39, 1.0).withOpacity(1);
-
   static final Color_Lightning = Colors.white.withOpacity(Engine.GoldenRatio_0_381);
-
-  static final Hue = Watch(0.0, onChanged: (double hueValue){
-     assert(hueValue >= 0);
-     hueValue = (hueValue) % 360.0;
-     Color_Start_Hue = hueValue;
-     Color_End_Hue = hueValue + Hue_Shift.value;
-  });
-
-  static final Color_Start = Watch(Default_Color_Start, onChanged: refreshShades);
-  static final Color_End = Watch(Default_Color_End, onChanged: refreshShades);
+  static final Hue = Watch(0.0, onChanged: onChangedHue);
+  static final Color_Start = Watch(Default_Color_Start, onChanged: onChangedColor);
+  static final Color_End = Watch(Default_Color_End, onChanged: onChangedColor);
   static final Hue_Shift = Watch(randomBetween(0, 360), onChanged: refreshHues);
   static final Hue_Offset = Watch(randomBetween(0, 360), onChanged: refreshHues);
 
-  static void refreshHues([double value = 0.0]){
-    Color_Start_Hue = Hue.value + Hue_Offset.value;
-    Color_End_Hue = Color_Start_Hue + Hue_Shift.value;
-  }
+  static final V0 = Watch(0.00, onChanged: onChangedLighting);
+  static final V1 = Watch(0.20, onChanged: onChangedLighting);
+  static final V2 = Watch(0.40, onChanged: onChangedLighting);
+  static final V3 = Watch(0.60, onChanged: onChangedLighting);
+  static final V4 = Watch(0.80, onChanged: onChangedLighting);
+  static final V5 = Watch(0.92, onChanged: onChangedLighting);
+  static final V6 = Watch(1.00, onChanged: onChangedLighting);
+
+  static final VArray = [
+    V0, V1, V2, V3, V4, V5, V6
+  ];
+
+  static final Color_Shades = Uint32List(7);
+  static final Transparent =  GameColors.black.withOpacity(0.5).value;
+
+  // GETTERS
 
   static double get Color_Start_Hue => HSVColor.fromColor(Color_Start.value).hue;
   static double get Color_End_Hue => HSVColor.fromColor(Color_End.value).hue;
+
+  // SETTERS
 
   static set Color_Start_Hue(double value) {
     assert(value >= 0);
@@ -43,22 +49,27 @@ class GameLighting {
     Color_End.value = Color_End.value.withOpacity(value);
   }
 
-  static final V0 = Watch(0.00, onChanged: refreshShades);
-  static final V1 = Watch(0.20, onChanged: refreshShades);
-  static final V2 = Watch(0.40, onChanged: refreshShades);
-  static final V3 = Watch(0.60, onChanged: refreshShades);
-  static final V4 = Watch(0.80, onChanged: refreshShades);
-  static final V5 = Watch(0.92, onChanged: refreshShades);
-  static final V6 = Watch(1.00, onChanged: refreshShades);
+  // REACTIONS
 
-  static final VArray = [
-    V0, V1, V2, V3, V4, V5, V6
-  ];
+  static void onChangedLighting(double v) => refreshShades();
+  static void onChangedColor(Color c) => refreshShades();
 
-  static final Color_Shades = Uint32List(7);
-  static final Transparent =  GameColors.black.withOpacity(0.5).value;
+  static void onChangedHue(double value){
+    assert(value >= 0);
+    value = (value) % 360.0;
+    Color_Start_Hue = value;
+    Color_End_Hue = value + Hue_Shift.value;
+  }
 
-  static void refreshShades([dynamic v]){
+  // METHODS
+
+  static void refreshHues([double value = 0.0]){
+    Color_Start_Hue = Hue.value + Hue_Offset.value;
+    Color_End_Hue = Color_Start_Hue + Hue_Shift.value;
+  }
+
+  /// EXPENSIVE OPERATION
+  static void refreshShades(){
     final start = HSVColor.fromColor(Color_Start.value);
     final end = HSVColor.fromColor(Color_End.value);
     Color_Shades[0] = HSVColor.lerp(start, end, V0.value)!.toColor().value;
@@ -70,3 +81,4 @@ class GameLighting {
     Color_Shades[6] = HSVColor.lerp(start, end, V6.value)!.toColor().value;
   }
 }
+
