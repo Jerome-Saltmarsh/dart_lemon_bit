8016e48ea69bfc835e7f7d1a8c730c8ac502a2de
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-02-03 09:53:45 +0800

    0.7.079

--- bleed-common/lib/src/version.dart
+++ bleed-common/lib/src/version.dart
@@ -1,4 +1,4 @@
-const version = '0.7.078';
+const version = '0.7.079';
 
 
 
--- gamestream_flutter/lib/functions/hsv_to_color.dart
+++ gamestream_flutter/lib/functions/hsv_to_color.dart
@@ -10,50 +10,23 @@ int hsvToColor({
   required int opacity,
 }) {
   // Calculate color
-  int r, g, b;
-  final i = (hue ~/ 43);
-  final f = (hue - (i * 43)) * 6;
   final p = (value * (255 - saturation)) >> 8;
-  final q = (value * (255 - ((saturation * f) >> 8))) >> 8;
-  final t = (value * (255 - ((saturation * (255 - f)) >> 8))) >> 8;
-  switch (i) {
+  final q = (value * (255)) >> 8;
+  final t = (value * (255 - ((saturation * (255)) >> 8))) >> 8;
+  switch (hue ~/ 43) {
     case 0:
-      r = value;
-      g = t;
-      b = p;
-      break;
+      return (opacity << 24) | (value << 16) | (t << 8) | p;
     case 1:
-      r = q;
-      g = value;
-      b = p;
-      break;
+      return (opacity << 24) | (q << 16) | (value << 8) | p;
     case 2:
-      r = p;
-      g = value;
-      b = t;
-      break;
+      return (opacity << 24) | (p << 16) | (value << 8) | t;
     case 3:
-      r = p;
-      g = q;
-      b = value;
-      break;
+      return (opacity << 24) | (p << 16) | (q << 8) | value;
     case 4:
-      r = t;
-      g = p;
-      b = value;
-      break;
+      return (opacity << 24) | (t << 16) | (p << 8) | value;
     case 5:
-      r = value;
-      g = p;
-      b = q;
-      break;
+      return (opacity << 24) | (value << 16) | (p << 8) | q;
     default:
-      r = 0;
-      g = 0;
-      b = 0;
-      break;
+      return (opacity << 24) | (0 << 16) | (0 << 8) | 0;
   }
-
-  // Combine color with opacity and return as integer
-  return (opacity << 24) | (r << 16) | (g << 8) | b;
 }
