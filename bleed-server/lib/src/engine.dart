157e287acbf650313c4956415d80765b957dc564
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-03-07 07:31:00 +0800

    0.7.456

--- bleed-server/lib/src/engine.dart
+++ bleed-server/lib/src/engine.dart
@@ -1,33 +1,33 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
 import 'package:bleed_server/gamestream.dart';
-import 'package:bleed_server/src/classes/src/game_environment.dart';
 import 'package:bleed_server/src/games/game_editor.dart';
 import 'package:bleed_server/src/io/save_directory.dart';
+import 'package:bleed_server/src/network/connection.dart';
 import 'package:bleed_server/src/scenes.dart';
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-import 'classes/src/game_time.dart';
 import 'constants/frames_per_second.dart';
-import 'network/websocket_server.dart';
 import 'system.dart';
 
 final engine = Engine();
 
 class Engine {
+
+  final connections = <Connection>[];
   final games = <Game>[];
   final scenes = Scenes();
+
   var frame = 0;
-  late GameTime officialTime;
-  late GameEnvironment environmentAboveGround;
-  late GameEnvironment environmentUnderground;
+  var connectionsTotal = 0;
 
   Future run() async {
     print('gamestream-version: $version');
-
     print('dart-version: ${Platform.version}');
-    print('gamestream.online server starting');
-    // print("Directory.current.path: ${Directory.current.path}");
 
     final sceneDirectoryExists = await Scene_Directory.exists();
 
@@ -36,23 +36,18 @@ class Engine {
     }
 
     if (isLocalMachine) {
-      print("Environment Detected: Jerome's Computer");
+      print("environment: Jerome's Computer");
     } else{
-      print("Environment Detected: Google Cloud Machine");
+      print("environment: Google Cloud");
     }
 
-    officialTime = GameTime();
-    environmentAboveGround = GameEnvironment();
-    environmentUnderground = GameEnvironment();
     await scenes.load();
 
-    Timer.periodic(Duration(milliseconds: 1000 ~/ framesPerSecond), fixedUpdate);
-    startWebsocketServer();
+    Timer.periodic(Duration(milliseconds: 1000 ~/ framesPerSecond), _fixedUpdate);
+    _startWebsocketServer();
   }
 
-  void fixedUpdate(Timer timer) {
-    environmentAboveGround.update();
-    officialTime.update();
+  void _fixedUpdate(Timer timer) {
     frame++;
 
     for (var i = 0; i < games.length; i++){
@@ -69,4 +64,36 @@ class Engine {
   void onGameCreated(Game game) {
     games.add(game);
   }
+
+  void _startWebsocketServer(){
+    print("startWebsocketServer()");
+    var handler = webSocketHandler(
+      onConnection,
+      protocols: ['gamestream.online'],
+      pingInterval: const Duration(seconds: 30),
+    );
+
+
+    shelf_io.serve(handler, '0.0.0.0', 8080).then((server) {
+      // server. = false;
+      print('Serving at wss://${server.address.host}:${server.port}');
+    }).catchError((error){
+      print("Websocket error occurred");
+      print(error);
+    });
+  }
+
+  void onConnection(WebSocketChannel webSocketChannel) {
+    final connection = Connection(webSocketChannel);
+    connections.add(connection);
+    connection.onDone = () => onConnectionDone(connection);
+    connectionsTotal++;
+    print("Connection Added. Current Connections: ${connections.length}, Total Connections: $connectionsTotal");
+  }
+
+  void onConnectionDone(Connection connection){
+    connections.remove(connection);
+    print("Connection Done. Current Connections: ${connections.length}, Total Connections: $connectionsTotal");
+  }
+
 }
--- bleed-server/lib/src/network/websocket_server.dart
+++ bleed-server/lib/src/network/websocket_server.dart
@@ -4,36 +4,3 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'connection.dart';
 
-final connections = <Connection>[];
-var connectionsTotal = 0;
-
-void startWebsocketServer(){
-  print("startWebsocketServer()");
-  var handler = webSocketHandler(
-    onConnection,
-    protocols: ['gamestream.online'],
-    pingInterval: const Duration(seconds: 30),
-  );
-
-
-  shelf_io.serve(handler, '0.0.0.0', 8080).then((server) {
-    // server. = false;
-    print('Serving at wss://${server.address.host}:${server.port}');
-  }).catchError((error){
-    print("Websocket error occurred");
-    print(error);
-  });
-}
-
-void onConnection(WebSocketChannel webSocketChannel) {
-  final connection = Connection(webSocketChannel);
-  connections.add(connection);
-  connection.onDone = () => onConnectionDone(connection);
-  connectionsTotal++;
-  print("Connection Added. Current Connections: ${connections.length}, Total Connections: $connectionsTotal");
-}
-
-void onConnectionDone(Connection connection){
-  connections.remove(connection);
-  print("Connection Done. Current Connections: ${connections.length}, Total Connections: $connectionsTotal");
-}
