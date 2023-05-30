19fff320a2adf3e867d96b9cc455da42ee9bab57
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-05-30 10:32:13 +0800

    0.8.245

--- bleed-server/lib/common/src/fight2d/game_fight2d_scene_generator.dart
+++ bleed-server/lib/common/src/fight2d/game_fight2d_scene_generator.dart
@@ -11,15 +11,47 @@ class GameFight2DSceneGenerator {
     final width = 40;
     final height = 20;
     final tiles = Uint8List(width * height);
-    for (var x = 0; x < width; x++) {
-      if (x > 5 && x < 8) continue;
-      for (var y = 0; y < height; y++) {
-        final index = x * height + y;
-        tiles[index] = y > height - 3
-            ? GameFight2DNodeType.Grass
-            : GameFight2DNodeType.Empty;
+
+    int getIndex(int x, int y) => x * height + y;
+
+    void setTile({required int x, required int y, required int type}){
+      if (x < 0) return;
+      if (x >= width) return;
+      if (y < 0) return;
+      if (y >= height) return;
+      tiles[getIndex(x, y)] = type;
+    }
+
+    void drawRectangle({
+      required int x,
+      required int y,
+      required int width,
+      required int height,
+      required int type,
+    }){
+      for (var xIndex = 0; xIndex < width; xIndex++) {
+        for (var yIndex = 0; yIndex < height; yIndex++) {
+          setTile(
+              x: x + xIndex,
+              y: y + yIndex,
+              type: type,
+          );
+        }
       }
     }
+
+    drawRectangle(
+      x: 10,
+      y: 7,
+      width: 6,
+      height: 2,
+      type: GameFight2DNodeType.Grass,
+    );
+
+    drawRectangle(x: 0, y: height - 3, width: width, height: 3, type: GameFight2DNodeType.Grass);
+    drawRectangle(x: 5, y: height - 3, width: 8, height: 3, type: GameFight2DNodeType.Empty);
+    drawRectangle(x: 6, y: height - 7, width: 6, height: 2, type: GameFight2DNodeType.Grass);
+
     return GameFight2DScene(tiles: tiles, width: width, height: height);
   }
 }
