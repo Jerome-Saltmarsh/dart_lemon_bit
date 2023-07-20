f393b47a8e617dfda1cd58b1bc3d48bd5caae9cd
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-07-20 08:09:04 +0200

    0.8.1201

--- gamestream_flutter/lib/common/src/version.dart
+++ gamestream_flutter/lib/common/src/version.dart
@@ -1,4 +1,4 @@
-const version = '0.8.1200';
+const version = '0.8.1201';
 
 
 
--- gamestream_flutter/lib/gamestream/isometric/components/render/classes/template_animation.dart
+++ gamestream_flutter/lib/gamestream/isometric/components/render/classes/template_animation.dart
@@ -1,71 +1,30 @@
 import 'dart:typed_data';
 import 'package:gamestream_flutter/common.dart';
 
+class WeaponSubType {
+  static const Melee_One_Handed = 0;
+  static const Melee_Two_Handed = 1;
+  static const Firearm_One_Handed = 3;
+  static const Firearm_Two_Handed = 2;
+  static const Bow = 3;
+}
+
 class TemplateAnimation {
 
   static const Frame_Changing = 4;
   static const Frame_Aiming_One_Handed = 7;
   static const Frame_Aiming_Two_Handed = 5;
   static const Frame_Aiming_Sword = 9;
+  static const Running1 = [12, 13, 14, 15];
+  static const Running2 = [16, 17, 18, 19];
+  static const Idle = [1];
+  static const Hurt = [3];
+  static const Changing = [1];
+  static const Firing_Bow = [
+    5, 5, 5, 5, 8, 8, 8, 8, 10, 5
+  ];
 
-  static final Uint8List Running1 = (){
-    final list = Uint8List(4);
-    list[0] = 12;
-    list[1] = 13;
-    list[2] = 14;
-    list[3] = 15;
-    return list;
-  }();
-
-  static final Uint8List Running2 = (){
-    final list = Uint8List(4);
-    list[0] = 16;
-    list[1] = 17;
-    list[2] = 18;
-    list[3] = 19;
-    return list;
-  }();
-
-  static Uint8List Idle = (){
-    final list = Uint8List(1);
-    list[0] = 1;
-    return list;
-  }();
-
-  static Uint8List Hurt = (){
-    final list = Uint8List(1);
-    list[0] = 3;
-    return list;
-  }();
-
-  static Uint8List Changing = (){
-    final list = Uint8List(1);
-    list[0] = 4;
-    return list;
-  }();
-
-  static Uint8List Firing_Bow = (){
-    final list = Uint8List(9);
-    list[0] = 5;
-    list[1] = 5;
-    list[2] = 5;
-    list[3] = 5;
-    list[4] = 8;
-    list[5] = 8;
-    list[6] = 8;
-    list[7] = 8;
-    list[8] = 10;
-    return list;
-  }();
-
-  static List<int> Firing_One_Handed_Firearm = (){
-    final frames = Uint8List(4);
-    frames[0] = 8;
-    frames[1] = 9;
-    frames[2] = 9;
-    frames[3] = 8;
-    return frames;
-  }();
+  static const Firing_One_Handed_Firearm = [8, 9, 9, 8];
 
   static const FiringShotgun = [
     6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 8, 8, 8, 8
@@ -80,21 +39,9 @@ class TemplateAnimation {
     10, 10, 10, 10, 10, 11
   ];
 
-  static const Throwing = [
-    10, 10, 10, 10, 10, 11
-  ];
+  static const Throwing = Punch;
 
-  static Uint8List StrikingBlade = (){
-    final list = Uint8List(7);
-    list[0] = 10;
-    list[1] = 10;
-    list[2] = 10;
-    list[3] = 11;
-    list[4] = 11;
-    list[5] = 11;
-    list[6] = 11;
-    return list;
-  }();
+  static const Striking_Blade = [10, 10, 10, 11];
 
   static List<int> getWeaponPerformAnimation(int weaponType){
 
@@ -128,12 +75,17 @@ class TemplateAnimation {
     }
 
     if (WeaponType.isMelee(weaponType)) {
-      return StrikingBlade;
+      return Striking_Blade;
     }
 
-    if (WeaponType.Bow == weaponType){
+    if (weaponType == WeaponType.Grenade){
+      return Throwing;
+    }
+
+    if (WeaponType.Bow == weaponType) {
       return Firing_Bow;
     }
+
     if (weaponType == WeaponType.Grenade) {
       return Punch;
     }
