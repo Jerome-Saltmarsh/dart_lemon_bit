d42d486bda17dc630a09cc7429e34e9a435e8d5f
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-08-17 12:44:37 +0200

    0.8.1757

--- gamestream_flutter/lib/gamestream/isometric/classes/sprite.dart
+++ gamestream_flutter/lib/gamestream/isometric/classes/sprite.dart
@@ -89,10 +89,13 @@ class Sprite {
       case AnimationMode.Single:
         return (row * columns) + min((columns * actionComplete).round(), columns - 1);
       case AnimationMode.Bounce:
-        final p = actionComplete < 0.5
-            ? actionComplete / 0.5
-            : (actionComplete - 0.5) / 0.5;
+        if (actionComplete < 0.5){
+          final p = actionComplete / 0.5;
+          return (row * columns) + min((columns * p).round(), columns - 1);
+        }
+        final p = 1.0 - ((actionComplete - 0.5) / 0.5);
         return (row * columns) + min((columns * p).round(), columns - 1);
+
       case AnimationMode.Loop:
         return (row * columns) + min((columns * actionComplete).round(), columns - 1);
       default:
--- gamestream_server/lib/common/src/version.dart
+++ gamestream_server/lib/common/src/version.dart
@@ -1,4 +1,4 @@
-const version = '0.8.1756';
+const version = '0.8.1757';
 
 
 
