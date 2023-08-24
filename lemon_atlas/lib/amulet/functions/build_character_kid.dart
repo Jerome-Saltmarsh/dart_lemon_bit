4688a053bfb5a953543fee65bb5110544df416b0
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-08-24 07:11:25 +0200

    0.8.1823

--- lemon_atlas/lib/amulet/functions/build_character_kid.dart
+++ lemon_atlas/lib/amulet/functions/build_character_kid.dart
@@ -1,9 +1,7 @@
 
 import 'dart:io';
 
-import 'package:image/image.dart';
 import 'package:lemon_atlas/atlas/functions/src.dart';
-import 'package:lemon_atlas/io/create_directory_if_not_exists.dart';
 
 import '../enums/character_state.dart';
 import '../enums/kid_part.dart';
@@ -24,7 +22,7 @@ void buildCharacterKid({
 
   exportSprite(
     sprite: sprite,
-    directory: 'C:/Users/Jerome/github/bleed/lemon_atlas/assets/sprites_3/kid/${part.groupName}/${part.fileName}',
+    directory: '${Directory.current.path}/assets/sprites_3/kid/${part.groupName}/${part.fileName}',
     name: state.name,
   );
 }
