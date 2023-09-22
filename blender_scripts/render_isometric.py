7f84f4953477bc56ed1d83d1b70cdc3e033de306
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-09-22 08:42:30 +0200

    0.8.2228

--- blender_scripts/render_isometric.py
+++ blender_scripts/render_isometric.py
@@ -2,8 +2,6 @@ import bpy
 import os
 import sys
 
-dir_renders = 'C:/Users/Jerome/github/bleed/lemon_atlas/assets/renders'
-
 
 def add_current_directory_to_path():
     current_dir = os.path.dirname(bpy.data.filepath)
@@ -12,20 +10,18 @@ def add_current_directory_to_path():
 
 
 def get_animation_tracks(object_name):
-    object_found = bpy.data.objects.get(object_name)
-    if object_found:
-        return object_found.animation_data.nla_tracks
-    raise ValueError('could not find animation object ' + object_name)
+    animation_object = get_object(object_name)
+    if animation_object:
+        return animation_object.animation_data.nla_tracks
+    raise ValueError('get_animation_tracks("' + object_name + '") is null')
 
 
-def get_unmuted_animation_tracks(object_name):
-    object_found = bpy.data.objects.get(object_name)
+def get_animation_tracks_unmuted(object_name):
     unmuted_tracks = []
-
-    if object_found and object_found.animation_data:
-        for animation_track in object_found.animation_data.nla_tracks:
-            if not animation_track.mute:
-                unmuted_tracks.append(animation_track)
+    animation_tracks = get_animation_tracks(object_name)
+    for animation_track in animation_tracks:
+        if not animation_track.mute:
+            unmuted_tracks.append(animation_track)
 
     return unmuted_tracks
 
@@ -76,81 +72,99 @@ def get_object(object_name):
     return bpy.data.objects.get(object_name)
 
 
+def get_collection(collection_name):
+    return bpy.data.collections.get(collection_name)
+
+
+def render():
+    bpy.ops.render.render(animation=True)
+
+
 def set_render_path(value):
     bpy.context.scene.render.filepath = value
 
 
-def prepare_render():
-    add_current_directory_to_path()
-    set_render_engine_eevee()
-    scene = bpy.context.scene
-    mesh_obj = get_object('Mesh Kid')
-    camera_tracks = get_unmuted_animation_tracks("Camera")
-    camera_pivot_tracks = get_animation_tracks("Camera Pivot")
-    animation_tracks = get_animation_tracks('Rig Kid')
+def get_animation_tracks_rig_kid():
+    return get_animation_tracks("Rig Kid")
 
+
+def set_animation_track_muted(object_name, track_name, value):
+    animation_tracks = get_animation_tracks(object_name)
+    if animation_tracks:
+        for animation_track in animation_tracks:
+            if animation_track.name == track_name:
+                animation_track.mute = value
+                return
+
+
+def set_render_frames(start, end):
+    scene = bpy.context.scene
+    scene.frame_start = start
+    scene.frame_end = end
+
+
+def unmute_camera_pivot_track(pivot_track_name):
+    camera_pivot_tracks = get_animation_tracks("Camera Pivot")
     if camera_pivot_tracks:
         for camera_pivot_track in camera_pivot_tracks:
-            camera_pivot_track.mute = True
+            camera_pivot_track.mute = camera_pivot_track.name != pivot_track_name
 
+
+def mute_animation_tracks(object_name):
+    animation_tracks = get_animation_tracks(object_name)
+    if animation_tracks:
+        for animation_track in animation_tracks:
+            animation_track.mute = True
+
+
+# BUSINESS LOGIC
+
+def prepare_render(camera_track):
+    set_render_engine_eevee()
+    hide_mesh_kid()
+    rig_kid_animation_tracks = get_animation_tracks_rig_kid()
+    mute_camera_pivot_tracks()
+    camera_track.mute = False
+
+    if camera_track.name == 'camera_front':
+        set_render_frames(1, 8)
+        unmute_camera_pivot_track('camera_1')
+        for rig_kid_animation_track in rig_kid_animation_tracks:
+            rig_kid_animation_track.mute = rig_kid_animation_track.name != 'idle'
+
+    if camera_track.name == 'camera_isometric':
+        set_render_frames(1, 64)
+        unmute_camera_pivot_track('camera_8')
+        for rig_kid_animation_track in rig_kid_animation_tracks:
+            rig_kid_animation_track.mute = rig_kid_animation_track.name == 'tpose'
+
+
+def mute_camera_pivot_tracks():
+    mute_animation_tracks("Camera Pivot")
+
+
+def hide_mesh_kid():
+    mesh_obj = get_object('Mesh Kid')
     if mesh_obj:
         mesh_obj.hide_render = True
 
-    if camera_tracks:
 
-        for camera_track in camera_tracks:
-            camera_track.mute = True
-
-        for camera_track in camera_tracks:
-            camera_track.mute = False
-            if camera_track.name == 'camera_front':
-                scene.frame_start = 1
-                scene.frame_end = 8
-
-                if camera_pivot_tracks:
-                    for camera_pivot_track in camera_pivot_tracks:
-                        camera_pivot_track.mute = camera_pivot_track.name != 'camera_1'
-
-                if animation_tracks:
-                    for animation_track in animation_tracks:
-                        animation_track.mute = animation_track.name != 'idle'
-
-            if camera_track.name == 'camera_isometric':
-                scene.frame_start = 1
-                scene.frame_end = 64
-
-                if camera_pivot_tracks:
-                    for camera_pivot_track in camera_pivot_tracks:
-                        camera_pivot_track.mute = camera_pivot_track.name != 'camera_8'
-
-                if animation_tracks:
-                    for animation_track in animation_tracks:
-                        animation_track.mute = animation_track.name == 'tpose'
-
-            camera_track.mute = True
-
-        for camera_track in camera_tracks:
-            camera_track.mute = False
+def render_mode_front(camera_track):
+    return camera_track.name == 'camera_front'
 
 
-def render_mode_front():
-    unmuted_tracks = get_unmuted_animation_tracks("Camera")
-    for unmuted_track in unmuted_tracks:
-        if unmuted_track.name == 'camera_front':
-            return True
-    return False
-
-
-def get_render_target():
-    if render_mode_front():
+def get_render_directory(camera_track):
+    dir_renders = 'C:/Users/Jerome/github/bleed/lemon_atlas/assets/renders'
+    if render_mode_front(camera_track):
         return dir_renders + '/front'
     else:
         return dir_renders + '/isometric'
 
 
-def perform_render():
-    rig_kid_animation_tracks = get_unmuted_animation_tracks("Rig Kid")
-    collections_export = bpy.data.collections.get("Exports")
+def render_camera_track(camera_track):
+    prepare_render(camera_track)
+    rig_kid_animation_tracks = get_animation_tracks_unmuted("Rig Kid")
+    collections_export = get_collection("Exports")
 
     if collections_export and rig_kid_animation_tracks:
         for track in rig_kid_animation_tracks:
@@ -171,12 +185,12 @@ def perform_render():
                         render_true(obj)
                         object_name = obj.name.replace(collection.name + "_", "")
                         mesh_directory = os.path.join(
-                            get_render_target() + "/kid/",
+                            get_render_directory(camera_track) + "/kid/",
                             collection.name, object_name, track.name
                         )
                         os.makedirs(mesh_directory, exist_ok=True)
                         set_render_path(os.path.join(mesh_directory, ""))
-                        bpy.ops.render.render(animation=True)
+                        render()
                         render_false(obj)
 
                     collection.hide_render = True
@@ -190,6 +204,19 @@ def perform_render():
             track.mute = False
 
 
-prepare_render()
-perform_render()
+def render_unmuted_camera_tracks():
+    unmuted_camera_tracks = get_animation_tracks_unmuted('Camera')
+
+    if not unmuted_camera_tracks:
+        raise ValueError('no active camera animations')
+
+    for unmuted_camera_track in unmuted_camera_tracks:
+        unmuted_camera_track.mute = True
+    for unmuted_camera_track in unmuted_camera_tracks:
+        render_camera_track(unmuted_camera_track)
+    for unmuted_camera_track in unmuted_camera_tracks:
+        unmuted_camera_track.mute = False
+
+
+render_unmuted_camera_tracks()
 set_render_path("c:/tmp")
--- gamestream_server/lib/packages/common/src/version.dart
+++ gamestream_server/lib/packages/common/src/version.dart
@@ -1,4 +1,4 @@
-const version = '0.8.2227';
+const version = '0.8.2228';
 
 
 
