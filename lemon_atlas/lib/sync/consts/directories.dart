66330f6ba67df471d139ac749bd45e3026cb5cc3
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-12-04 07:55:05 +0100

    0.8.2912

--- lemon_atlas/lib/sync/consts/directories.dart
+++ lemon_atlas/lib/sync/consts/directories.dart
@@ -1,2 +1,2 @@
 const dirRenders = 'C:/Users/Jerome/github/amulet/lemon_atlas/assets/renders';
-const dirSprites = 'C:/Users/Jerome/github/amulet/amulet_flutter/sprites';
+const dirSprites = 'C:/Users/Jerome/github/amulet/amulet_flutter/assets/sprites';
