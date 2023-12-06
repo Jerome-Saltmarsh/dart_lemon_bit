d7bfd359fd39400860025d6f842fb0c13f8b6377
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-12-06 17:45:31 +0100

    0.8.2927

--- amulet_flutter/lib/amulet/ui/widgets/amulet_world_map.dart
+++ amulet_flutter/lib/amulet/ui/widgets/amulet_world_map.dart
@@ -1,8 +1,9 @@
-import 'package:amulet_flutter/isometric/functions/get_render.dart';
-import 'package:flutter/material.dart';
-import 'package:amulet_flutter/amulet/amulet.dart';
-import 'package:lemon_engine/lemon_engine.dart';
 import 'package:amulet_engine/packages/lemon_math.dart';
+import 'package:amulet_flutter/amulet/amulet.dart';
+import 'package:amulet_flutter/isometric/functions/get_render.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:lemon_engine/lemon_engine.dart';
 
 class AmuletWorldMap extends StatelessWidget {
   final Amulet amulet;
@@ -21,6 +22,12 @@ class AmuletWorldMap extends StatelessWidget {
   var screenWidth = 0.0;
   var screenHeight = 0.0;
   var zoom = 1.0;
+  var scrollSensitivity = 0.00025;
+
+  static const zoomMin = 0.2;
+  static const zoomMax = 6.0;
+
+
 
   AmuletWorldMap({
     super.key,
@@ -28,59 +35,59 @@ class AmuletWorldMap extends StatelessWidget {
     required this.size,
   });
 
+  void _internalOnPointerSignal(PointerSignalEvent event) {
+    if (event is PointerScrollEvent) {
+      zoom -=  event.scrollDelta.dy * scrollSensitivity;
+      zoom = zoom.clamp(zoomMin, zoomMax);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     print('amuletWorldMap.build()');
 
-    final canvas = CustomCanvas(
-      paint: (canvas, canvasSize) {
-        final amulet = this.amulet;
-        final worldMapPicture = amulet.worldMapPicture;
-        if (worldMapPicture == null) {
-          return;
-        }
+    final canvas = Listener(
+      onPointerSignal: _internalOnPointerSignal,
+      child: CustomCanvas(
+        paint: (canvas, canvasSize) {
+          final amulet = this.amulet;
+          final worldMapPicture = amulet.worldMapPicture;
+          if (worldMapPicture == null) {
+            return;
+          }
 
-        screenWidth = canvasSize.width;
-        screenHeight = canvasSize.height;
+          screenWidth = canvasSize.width;
+          screenHeight = canvasSize.height;
 
-        canvas.scale(zoom, zoom);
-        const mapSize = 100.0;
-        final canvasWidth = canvasSize.width;
-        final canvasHeight = canvasSize.height;
-        final centerX = canvasWidth * 0.5;
-        final centerY = canvasHeight * 0.5;
-        final player = amulet.player;
-        final position = player.position;
-        final scene = amulet.scene;
-        final ratioX = position.x / scene.lengthRows;
-        final ratioY = position.y / scene.lengthColumns;
-        final posX = amulet.worldRow * mapSize + (mapSize * ratioX);
-        final posY = amulet.worldColumn * mapSize + (mapSize * ratioY);
-        paint.color = Colors.white;
+          canvas.scale(zoom, zoom);
+          const mapSize = 100.0;
+          final player = amulet.player;
+          final position = player.position;
+          final scene = amulet.scene;
+          final ratioX = position.x / scene.lengthRows;
+          final ratioY = position.y / scene.lengthColumns;
+          final posX = amulet.worldRow * mapSize + (mapSize * ratioX);
+          final posY = amulet.worldColumn * mapSize + (mapSize * ratioY);
+          paint.color = Colors.white;
 
-        final playerRenderX = getRenderX(posX, posY);
-        final playerRenderY = getRenderY(posX, posY, 0);
-
-        // final cameraX = playerRenderX - (centerX / zoom);
-        // final cameraY = playerRenderY - (centerY / zoom);
-        canvas.rotate(piQuarter);
-        canvas.translate(-cameraX, -cameraY);
-        /// TODO Memory leak
-        final renderPos = Offset(getRenderX(posX, posY), getRenderY(posX, posY, 0));
-        canvas.drawImage(worldMapPicture, const Offset(0, 0), paint);
-        canvas.rotate(-piQuarter);
-        canvas.drawCircle(renderPos, 1, paint);
-        textPainter.text = textSpanHello;
-        textPainter.layout();
-        textPainter.paint(canvas, renderPos);
-        paint.color = Colors.blue;
-
-        cameraFollow(
-          playerRenderX,
-          playerRenderY,
-          0.001,
-        );
-      },
+          targetX = getRenderX(posX, posY);
+          targetY = getRenderY(posX, posY, 0);
+          canvas.translate(-cameraX, -cameraY);
+          canvas.rotate(piQuarter);
+          /// TODO Memory leak
+          final targetOffset = Offset(targetX, targetY);
+          canvas.drawImage(worldMapPicture, const Offset(0, 0), paint);
+          // canvas.rotate(-piQuarter);
+          canvas.drawCircle(targetOffset, 1, paint);
+          canvas.drawCircle(Offset(screenCenterWorldX, screenCenterWorldY), 4, paint);
+          // canvas.drawCircle(Offset(cameraX, cameraY), 6, paint);
+          textPainter.text = textSpanHello;
+          textPainter.layout();
+          textPainter.paint(canvas, targetOffset);
+          paint.color = Colors.blue;
+          cameraFollowTarget(0.0005);
+        },
+      ),
     );
 
     return Container(
@@ -103,9 +110,9 @@ class AmuletWorldMap extends StatelessWidget {
     );
   }
 
-  void cameraFollow(double x, double y, [double speed = 0.00075]) {
-    final diffX = screenCenterWorldX - x;
-    final diffY = screenCenterWorldY - y;
+  void cameraFollowTarget([double speed = 0.00075]) {
+    final diffX = screenCenterWorldX - targetX;
+    final diffY = screenCenterWorldY - targetY;
     cameraX -= (diffX * 75) * speed;
     cameraY -= (diffY * 75) * speed;
   }
