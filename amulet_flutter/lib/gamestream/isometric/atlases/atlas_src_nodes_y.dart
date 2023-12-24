5c98442ff74e033ae0b8a32eb95c13f06e41da61
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-12-24 08:46:20 +0100

    0.8.3023

--- amulet_flutter/lib/gamestream/isometric/atlases/atlas_src_nodes_y.dart
+++ amulet_flutter/lib/gamestream/isometric/atlases/atlas_src_nodes_y.dart
@@ -1,8 +1,8 @@
 import 'package:amulet_engine/packages/common.dart';
 
 const nodeTypeSrcY = <int, double> {
-  NodeType.Brick: 1760,
-  NodeType.Grass: 1808,
-  NodeType.Soil: 1856,
-  NodeType.Wood: 1904,
+  NodeType.Brick: 48,
+  NodeType.Grass: 96,
+  NodeType.Soil: 144,
+  NodeType.Wood: 192,
 };
