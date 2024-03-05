378f18063eb8c2410c6538900af31665adfd32a2
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2024-03-05 10:36:24 +1000

    0.8.3663

--- amulet_flutter/lib/amulet/getters/get_skill_type_level_description.dart
+++ amulet_flutter/lib/amulet/getters/get_skill_type_level_description.dart
@@ -26,14 +26,17 @@ String? getSkillTypeLevelDescription(SkillType skillType, int level) {
     case SkillType.Strike:
       return '-';
     case SkillType.Frostball:
-      return 'ice damage: ${SkillType.getDamageFrostBall(level).toStringAsFixed(1)}\n'
-          'freeze damage: ${SkillType.getAilmentDamageFrostBall(level).toStringAsFixed(1)}/s\n'
-          'freeze duration: ${SkillType.getAilmentDurationFrostBall(level).toStringAsFixed(1)}s';
+      return formatIceDamage(
+        damage: SkillType.getDamageFrostBall(level),
+        duration: SkillType.getAilmentDurationFrostBall(level),
+        durationDamage: SkillType.getAilmentDamageFrostBall(level),
+      );
     case SkillType.Fireball:
-      return 'fire damage: ${SkillType.getDamageFireball(level).toStringAsFixed(1)}\n'
-          'burn damage: ${SkillType.getAilmentDamageFireball(level).toStringAsFixed(1)}/s\n'
-          'burn duration: ${SkillType.getAilmentDurationFireball(level).toStringAsFixed(1)}s'
-       ;
+      return formatFireDamage(
+        damage: SkillType.getDamageFireball(level),
+        duration: SkillType.getAilmentDurationFireball(level),
+        durationDamage: SkillType.getAilmentDamageFireball(level),
+      );
     case SkillType.Explode:
       return 'damage: ${SkillType.getDamageExplode(level)}';
     case SkillType.Shoot_Arrow:
@@ -41,9 +44,17 @@ String? getSkillTypeLevelDescription(SkillType skillType, int level) {
     case SkillType.Split_Shot:
       return 'total arrows: ${SkillType.getSplitShotTotalArrows(level)}';
     case SkillType.Ice_Arrow:
-      return 'ice damage: ${SkillType.getDamageIceArrow(level)}';
+      return formatIceDamage(
+        damage: SkillType.getDamageIceArrow(level),
+        duration: SkillType.getAilmentDurationIceArrow(level),
+        durationDamage: SkillType.getAilmentDamageIceArrow(level),
+      );
     case SkillType.Fire_Arrow:
-      return 'fire damage: ${SkillType.getDamageFireArrow(level)}';
+      return formatFireDamage(
+        damage: SkillType.getDamageFireArrow(level),
+        duration: SkillType.getAilmentDurationFireArrow(level),
+        durationDamage: SkillType.getAilmentDamageFireArrow(level),
+      );
     case SkillType.Area_Damage:
       return '${SkillType.getAreaDamage(level).toStringPercentage} area damage';
     case SkillType.Run_Speed:
@@ -52,3 +63,40 @@ String? getSkillTypeLevelDescription(SkillType skillType, int level) {
       return 'range +${SkillType.getRangeWindCut(level).toInt()}';
   }
 }
+
+String formatFireDamage({
+  required double damage,
+  required double duration,
+  required double durationDamage,
+}) =>
+    formatAilment(
+      damageType: 'fire',
+      ailmentType: 'burn',
+      damage: damage,
+      duration: duration,
+      durationDamage: durationDamage
+    );
+
+String formatIceDamage({
+  required double damage,
+  required double duration,
+  required double durationDamage,
+}) =>
+    formatAilment(
+      damageType: 'ice',
+      ailmentType: 'freeze',
+      damage: damage,
+      duration: duration,
+      durationDamage: durationDamage
+    );
+
+String formatAilment({
+  required String damageType,
+  required String ailmentType,
+  required double damage,
+  required double duration,
+  required double durationDamage,
+}) =>
+    '$damageType damage: ${damage.toStringAsFixed(1)}\n'
+    '$ailmentType damage: ${duration.toStringAsFixed(1)}/s\n'
+    '$ailmentType duration: ${duration.toStringAsFixed(1)}s';
