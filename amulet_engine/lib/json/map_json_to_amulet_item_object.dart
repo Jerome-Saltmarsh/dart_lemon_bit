a1265c3136833cc45ff3fde6740508b69aa42509
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2024-03-08 07:41:45 +1000

    0.8.3728

--- amulet_engine/lib/json/map_json_to_amulet_item_object.dart
+++ amulet_engine/lib/json/map_json_to_amulet_item_object.dart
@@ -16,15 +16,6 @@ AmuletItemObject? mapJsonToAmuletItemObject(Json? json) {
     return null;
   }
 
-  final skillPointsText = json.getMapStringInt(AmuletField.Skill_Points);
-  final skillPoints = <SkillType, int> {};
-
-  for (final entry in skillPointsText.entries) {
-    final skillType = SkillType.tryParse(entry.key);
-    if (skillType == null) continue;
-    skillPoints[skillType] = entry.value;
-  }
-
   return AmuletItemObject(
       amuletItem: amuletItem,
       level: json.getInt(AmuletField.Level),
